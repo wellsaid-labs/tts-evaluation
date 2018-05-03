26d19525cbd72e54627f0ee83d1f97559fbc5694
Michael Petrochuk <michaelp@allenai.org>
2018-05-02 21:14:45 -0700

    Update tests

--- tests/test_spectrogram.py
+++ tests/test_spectrogram.py
@@ -1,15 +1,11 @@
-from functools import partial
-
 import argparse
 import mock
 import os
 
-import pytest
 import numpy as np
 import tensorflow as tf
 
 from tensorflow.contrib.framework.python.ops import audio_ops
-from tensorflow.contrib.signal.python.ops import window_ops
 
 from src.spectrogram import _read_audio
 from src.spectrogram import wav_to_log_mel_spectrogram
@@ -17,7 +13,6 @@ from src.spectrogram import log_mel_spectrogram_to_wav
 from src.spectrogram import command_line_interface
 
 
-@pytest.mark.skip()
 def test_librosa_tf_decode_wav():
     """ Librosa provides a more flexible API for decoding WAVs. To ensure consistency with TF, we
     test the output is the same.
@@ -35,7 +30,6 @@ def test_librosa_tf_decode_wav():
 @mock.patch(
     'argparse.ArgumentParser.parse_args',
     return_value=argparse.Namespace(path='tests/_test_data/lj_speech.wav'))
-@pytest.mark.skip()
 def test_command_line_interface(_):
     command_line_interface()
     assert os.path.isfile('tests/_test_data/lj_speech_spectrogram.png')
@@ -46,7 +40,6 @@ def test_command_line_interface(_):
 
 @mock.patch(
     'argparse.ArgumentParser.parse_args', return_value=argparse.Namespace(path='tests/_test_data/'))
-@pytest.mark.skip()
 def test_command_line_interface_multiple_files(_):
     command_line_interface()
     assert os.path.isfile('tests/_test_data/lj_speech_spectrogram.png')
@@ -57,7 +50,6 @@ def test_command_line_interface_multiple_files(_):
     os.remove('tests/_test_data/voice_over_spectrogram.png')
 
 
-@pytest.mark.skip()
 def test_wav_to_log_mel_spectrogram_smoke():
     """ Smoke test to ensure everything runs.
     """
@@ -73,7 +65,7 @@ def test_log_mel_spectrogram_to_wav_smoke():
     wav_filename = 'tests/_test_data/lj_speech.wav'
     new_wav_filename = 'tests/_test_data/lj_speech_reconstructed.wav'
     log_mel_spectrogram = wav_to_log_mel_spectrogram(wav_filename)
-    log_mel_spectrogram_to_wav(log_mel_spectrogram, new_wav_filename)
+    log_mel_spectrogram_to_wav(log_mel_spectrogram, new_wav_filename, log=True)
 
     assert os.path.isfile(new_wav_filename)
 
