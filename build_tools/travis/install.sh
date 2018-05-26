a460f31ebac03f40f1d76fd7fd9aa11d197923b3
Michael Petrochuk <petrochukm@allenai.org>
2018-05-25 20:57:24 -0700

    Update build files

--- build_tools/travis/install.sh
+++ build_tools/travis/install.sh
@@ -20,7 +20,7 @@ fi
 # TODO: Add a script similar to Travis to test locally with virtual environment
 
 # Install requirements via pip
-pip install -r requirements.txt --quiet --upgrade
+pip install -r requirements.txt --quiet --upgrade --user
 
 # Install PyTorch Dependancies
 if [[ $TRAVIS_PYTHON_VERSION == '3.6' ]]; then
