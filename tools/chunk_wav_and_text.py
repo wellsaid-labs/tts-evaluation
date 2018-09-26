952360784420bdc80bcbbf37d752df210fde3e23
Michael Petrochuk <petrochukm@allenai.org>
2018-09-25 17:04:03 -0700

    V2 - Refactor script, still works

--- tools/chunk_wav_and_text.py
+++ tools/chunk_wav_and_text.py
@@ -3,7 +3,7 @@ import logging
 import pathlib
 import requests
 
-from functools import partial
+from collections import namedtuple
 
 import pandas
 import librosa
@@ -11,6 +11,21 @@ import librosa
 from src.audio import read_audio
 
 logger = logging.getLogger(__name__)
+
+GENTLE_SUCCESS_CASE = 'success'
+""" An alignment that was determined confidently between characters and text.
+
+Args:
+    start_audio (int): Start of the audio in samples.
+    end_audio (int): End of audio in samples.
+    start_text (int): Start of text in characters.
+    end_text (int): End of text in characters.
+    next_start_text (int): Start of next alignement text in characters or end of text.
+    next_start_audio (int): Start of next alignement audio in samples or end of audio.
+"""
+Alignment = namedtuple(
+    'Alignment',
+    ['start_audio', 'end_audio', 'start_text', 'end_text', 'next_start_text', 'next_start_audio'])
 
 
 def _request_gentle(wav_path,
@@ -35,8 +50,7 @@ def _request_gentle(wav_path,
                 {
                     alignedWord (str): Normalized and aligned word.
                     word (str): Parallel unnormalized word from transcript.
-                    case (str): One of three cases 'success', 'not-found-in-audio', and
-                        'not-found-in-transcript'.
+                    case (str): One of two cases 'success' or 'not-found-in-audio'.
                     start (float): Time the speakers begins to speak ``word`` in seconds.
                     end (float): Time the speakers stops speaking ``word`` in seconds.
                     startOffset (int): The trascript character index of the first letter of word.
@@ -69,131 +83,173 @@ def _request_gentle(wav_path,
             logger.warn('``alignedWord`` does not exist — %s', word)
         elif word['alignedWord'] != word['word'].lower():
             logger.warn('``alignedWord`` does not match trascript ``word`` — %s', word)
-    unaligned_words = sum([w['case'] != 'success' for w in response['words']])
+    unaligned_words = sum([w['case'] != GENTLE_SUCCESS_CASE for w in response['words']])
     if unaligned_words > 0:
         logger.warn('%f%% unaligned words', (unaligned_words / len(response['words']) * 100))
 
     return response
 
 
-def align_wav_and_scripts(wav_path, scripts, sample_rate=44100):
+def align_wav_and_scripts(wav_path, scripts, sample_rate):
     """ Align an audio file with the scripts spoken in the audio.
+
+    Notes:
+        - The returned alignments do not include the entire script. We only include verified
+          and confident alignments.
 
     Args:
         wav_path (Path): The audio file path.
         scripts (list of str): The scripts spoken in the audio.
-        sample_rate (int, optional)
+        sample_rate (int): Sample rate of the audio.
 
     Returns:
-        (list) [
-            (list) [  # For every script, a list of words and their alignment.
-               {
-                    alignedWord (str): Normalized and aligned word.
-                    word (str): Parallel unnormalized word from transcript.
-                    case (str): One of three cases 'success', 'not-found-in-audio', and
-                        'not-found-in-transcript'.
-                    start (float): Time the speakers begins to speak ``word`` in samples.
-                    end (float): Time the speakers stops speaking ``word`` in samples.
-                    startOffset (int): The script character index of the first letter of word.
-                    endOffset (int): The script character index plus one of the last letter of word.
-                }, ...
-            ]
-        ]
+        (list of Alignment): For every script, a list of found alignments.
     """
+    seconds_to_samples = lambda seconds: int(round(seconds * sample_rate))
+    max_sample_rate = seconds_to_samples(
+        librosa.get_duration(filename=str(wav_path), sr=sample_rate))
     transcript = '\n'.join(scripts)
     alignment = _request_gentle(wav_path, transcript)
 
     # Align seperate scripts with the transcript alignment.
-    aligned_words = alignment['words']
+    aligned_words = [w for w in alignment['words'] if w['case'] == GENTLE_SUCCESS_CASE]
     transcript_start_offset = 0
-    scripts_alignment = []
+    all_scripts_alignments = []
     for script in scripts:
-        script_words = []
-        while (len(aligned_words) > 0 and
-               aligned_words[0]['endOffset'] <= transcript_start_offset + len(script)):
+        scripts_alignments = []
+        transcript_end_offset = transcript_start_offset + len(script)
+        while (len(aligned_words) > 0 and aligned_words[0]['endOffset'] <= transcript_end_offset):
             aligned_word = aligned_words.pop(0)
-            aligned_word['startOffset'] -= transcript_start_offset
 
-            # Document this change
             if len(aligned_words) > 1:
-                aligned_word[
-                    'endOffset'] = aligned_words[0]['startOffset'] - transcript_start_offset - 1
-                assert aligned_word['endOffset'] > aligned_word['startOffset']
+                next_start_text = aligned_words[0]['startOffset'] - transcript_start_offset
+                next_start_audio = seconds_to_samples(aligned_words[0]['start'])
             else:
-                aligned_word['endOffset'] = len(script)
-                assert aligned_word['endOffset'] > aligned_word['startOffset']
+                next_start_text = len(script)
+                next_start_audio = max_sample_rate
 
-            if aligned_word['case'] == 'success':
-                del aligned_word['phones']
-                aligned_word['start'] = int(round(aligned_word['start'] * sample_rate))
-                aligned_word['end'] = int(round(aligned_word['end'] * sample_rate))
-            script_words.append(aligned_word)
-        scripts_alignment.append(script_words)
+            scripts_alignments.append(
+                Alignment(
+                    start_text=aligned_word['startOffset'] - transcript_start_offset,
+                    end_text=aligned_word['endOffset'] - transcript_start_offset,
+                    start_audio=seconds_to_samples(aligned_word['start']),
+                    end_audio=seconds_to_samples(aligned_word['end']),
+                    next_start_text=next_start_text,
+                    next_start_audio=next_start_audio))
 
+        all_scripts_alignments.append(scripts_alignments)
         transcript_start_offset += len(script) + 1  # Add plus one for ``\n``
-    assert len(scripts_alignment) == len(
+
+    assert len(all_scripts_alignments) == len(
         scripts), 'Alignment was not able to provide alignment for all scripts.'
-    return scripts_alignment
+    assert len(aligned_words) == 0, 'Not all words were aligned with a script.'
+    return all_scripts_alignments
 
 
-def compute_silence(sequence, index):
-    return (sequence[index - 1]['end'] - sequence[index]['start']) / (
-        sequence[index - 1]['endOffset'] - sequence[index]['startOffset'])
+def chunk_alignments(alignments, sample_rate, max_chunk_length):
+    """ Chunk audio and text to be less than ``max_chunk_length``.
 
+    Notes:
+        - We chunk based on the largest silence between alignments. We assume that the returned
+          chunks are independent because there exists silence between chunks.
+        - Ideal chunk independence means that prior text does not affect the voice actors
+          interpretation of that chunk of text.
 
-def main(wav_path, csv_path, text_column='Content', max_clip_length=10):
+    Args:
+        alignments (list of Alignment): List of alignments between text and audio.
+        sample_rate (int): Sample rate of the audio.
+        max_chunk_length (float): Number of seconds for the maximum chunk audio length.
+
+    Returns:
+        text_spans (list of tuple(int, int)): List of spans of text as measured in characters.
+        audio_spans (list of tuple(int, int)): List of spans of audio as measured in samples.
     """
+    # Compute the average samples of silence per character
+    average_silence = (
+        lambda a: (a[1].next_start_audio - a[1].end_audio) / (a[1].next_start_text - a[1].end_text))
+
+    chunks = []
+    largest_chunk = alignments
+    largest_chunk_length = largest_chunk[-1].next_start_audio - largest_chunk[0].start_audio
+    while len(largest_chunk) > 1 and largest_chunk_length > max_chunk_length * sample_rate:
+        # TODO: Keep a list of the largest silences instead of recomputing every iteration
+        # NOTE: Computes the largest average silence per character.
+        largest_silence_index, _ = max(enumerate(largest_chunk[:-1]), key=average_silence)
+
+        chunks.append(largest_chunk[:largest_silence_index + 1])
+        chunks.append(largest_chunk[largest_silence_index + 1:])
+
+        # TODO: Keep a list of the largest chunks instead of recomputing every iteration
+        largest_chunk_index, _ = max(
+            enumerate(chunks), key=lambda c: c[1][-1].next_start_audio - c[1][0].start_audio)
+        largest_chunk = chunks.pop(largest_chunk_index)
+        largest_chunk_length = largest_chunk[-1].next_start_audio - largest_chunk[0].start_audio
+
+    chunks.append(largest_chunk)
+    chunks = sorted(chunks, key=lambda c: c[0].start_text)
+    text_spans = []
+    audio_spans = []
+    for i, chunk in enumerate(chunks):
+        # NOTE: Given that alignments do not cover all the text, we extend the spans to the next
+        # start.
+        # TODO: Should we cut the audio up to next_start_audio, end_audio or in between? If there
+        # are unaligned words then next_start_audio makes sense. If next_start_audio isn't
+        # quite accurate then in between makes sense.
+        # TODO: Consider not expanding the text passed the end_text except in cases of punctutation;
+        # look at other cases where alignment does not occur.
+        text_spans.append((chunk[0].start_text, chunk[-1].next_start_text))
+        audio_spans.append((chunk[0].start_audio, chunk[-1].next_start_audio))
+
+    return text_spans, audio_spans
+
+
+def main(wav_path,
+         csv_path,
+         text_column='Content',
+         wav_column='WAV Filename',
+         destination='data/processed',
+         wav_directory_name='wav',
+         sample_rate=44100,
+         max_chunk_length=10):
+    """ Align audio with scripts, then create and save chunks of audio and text.
+
     Args:
         wav_path (Path): The audio file path.
         csv_path (Path): The CSV file path containing metadata associated with audio file.
         text_column (str, optional): The script column in the CSV file.
-        max_clip_length (float, optional): max clip length in seconds.
+        wav_column (str, optional): Column name for the new audio filename column.
+        wav_directory_name (str, optional): The directory name to store audio clips.
+        sample_rate (int, optional): Sample rate of the audio.
+        max_chunk_length (float, optional): Number of seconds for the maximum chunk audio length.
     """
     assert wav_path.is_file(), 'Audio file must exist'
     assert csv_path.is_file(), 'CSV file must exist'
 
-    audio = read_audio(str(wav_path), 44100)
-    df = pandas.read_csv(csv_path)
-    scripts = [x.strip() for x in df[text_column]]
-    df['alignment'] = align_wav_and_scripts(wav_path, scripts)
-
-    text_directory = pathlib.Path('data/text')
-    text_directory.mkdir(parents=True, exist_ok=True)
-
-    wav_directory = pathlib.Path('data/wav')
+    root_directory = pathlib.Path(destination)
+    root_directory.mkdir(parents=True, exist_ok=True)
+    metadata_filename = root_directory / csv_path.name  # Filename to store CSV metadata
+    wav_directory = root_directory / wav_directory_name  # Directory to store clips
     wav_directory.mkdir(parents=True, exist_ok=True)
 
+    audio = read_audio(str(wav_path), sample_rate)
+    df = pandas.read_csv(csv_path)
+    scripts = [x.strip() for x in df[text_column]]
+    alignments = align_wav_and_scripts(wav_path, scripts, sample_rate=sample_rate)
+
+    to_write = []
     for i, row in df.iterrows():
-        chunks = []
-        max_chunk = [word for word in row['alignment'] if word['case'] == 'success']
-        max_chunk_length = max_chunk[-1]['end'] - max_chunk[0]['start']
-        while len(max_chunk) > 1 and max_chunk_length > max_clip_length * 44100:
-            # TODO: Do something about the fact that not all have a start_end but that doesn't
-            # mean there is a large silence there...
-            max_silence_index = max(
-                range(1, len(max_chunk)), key=partial(compute_silence, max_chunk))
+        text_spans, audio_spans = chunk_alignments(
+            alignments[i], sample_rate=sample_rate, max_chunk_length=max_chunk_length)
 
-            chunks.append(max_chunk[:max_silence_index])
-            chunks.append(max_chunk[max_silence_index:])
+        for j, (text_span, audio_span) in enumerate(zip(text_spans, audio_spans)):
+            new_row = row.to_dict()
+            new_row[text_column] = row[text_column][slice(*text_span)].strip()
+            new_row[wav_column] = str(wav_directory / ('SCRIPT_%d_CHUNK_%d.wav' % (i, j)))
+            to_write.append(new_row)
+            audio_span = audio[slice(*audio_span)]
+            librosa.output.write_wav(new_row[wav_column], audio_span, sr=sample_rate)
 
-            max_chunk_index = max(
-                range(len(chunks)), key=lambda i: chunks[i][-1]['end'] - chunks[i][0]['start'])
-            max_chunk = chunks.pop(max_chunk_index)
-            max_chunk_length = max_chunk[-1]['end'] - max_chunk[0]['start']
-
-        chunks.append(max_chunk)
-
-        for j, chunk in enumerate(chunks):
-            # TODO: endOffset should be -1 of the next word in case anything like periods were
-            # missed
-            script = row[text_column]
-            text_chunk = script[chunk[0]['startOffset']:min(chunk[-1]['endOffset'] +
-                                                            1, len(script))]
-            assert chunk[-1]['end'] <= audio.shape[0]
-            audio_chunk = audio[chunk[0]['start']:chunk[-1]['end']]
-            librosa.output.write_wav(
-                str(wav_directory / ('%d_%d.wav' % (i, j))), audio_chunk, sr=44100)
-            (text_directory / ('%d_%d.txt' % (i, j))).write_text(text_chunk)
+    pandas.DataFrame(to_write).to_csv(str(metadata_filename))
 
 
 if __name__ == "__main__":
