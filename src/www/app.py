563bdb6a18aafa98c0e724ce9764adc644e56c20
Michael Petrochuk <petrochukm@allenai.org>
2018-10-04 14:57:46 -0700

    Fix errors

--- src/www/app.py
+++ src/www/app.py
@@ -12,6 +12,8 @@ from flask import jsonify
 from flask import request
 from flask import send_file
 
+import librosa
+
 from src.audio import griffin_lim
 from src.bin.feature_model._utils import set_hparams as set_feature_model_hparams
 from src.bin.signal_model._utils import set_hparams as set_signal_model_hparams
@@ -20,17 +22,18 @@ from src.utils import load_checkpoint
 app = Flask(__name__)
 logger = logging.getLogger(__name__)
 logging.basicConfig(level=logging.INFO)
-torch.set_grad_enabled(False)
 
-SAMPLES_FOLDER = Path('static/samples')
-DEFAULT_SAMPLE_FILENAME = 'voiceover.mp3'
+SAMPLES_FOLDER = Path('src/www/static/samples')
+DEFAULT_SAMPLE_FILENAME = 'voiceover.wav'
+
 
 def load_feature_model(path):
     """ Load the feature model from a ``Path`` or ``str`` path argument """
     set_feature_model_hparams()
     checkpoint = load_checkpoint(path, torch.device('cuda'))
     logger.info('Loaded feature model at step %d', checkpoint['step'])
-    return checkpoint['text_encoder'], checkpoint['model'].eval()
+    return checkpoint['model'].eval(), checkpoint['text_encoder']
+
 
 def load_signal_model(path):
     """ Load the signal model from a ``Path`` or ``str`` path argument """
@@ -39,13 +42,14 @@ def load_signal_model(path):
     logger.info('Loaded signal model at step %d', checkpoint['step'])
     return checkpoint['model'].eval()
 
+
 EXPERIMENT_ROOT = Path('/home/michaelp/WellSaid-Labs-Text-To-Speech/experiments/')
-FEATURE_MODEL, TEXT_ENCODER = load_feature_model(EXPERIMENT_ROOT /
-                'feature_model/09_16/post_net_no_dropout/' /
-                'checkpoints/1538001059/step_289899.pt')
-SIGNAL_MODEL = load_signal_model(EXPERIMENT_ROOT /
-                'signal_model/09_28/feature_model_post_net_no_dropout/' /
-                'checkpoints/1538601423/step_2217350.pt')
+FEATURE_MODEL, TEXT_ENCODER = load_feature_model(
+    EXPERIMENT_ROOT / 'feature_model/09_16/post_net_no_dropout/' /
+    'checkpoints/1538001059/step_289899.pt')
+SIGNAL_MODEL = load_signal_model(
+    EXPERIMENT_ROOT / 'signal_model/09_28/feature_model_post_net_no_dropout/' /
+    'checkpoints/1538601423/step_2217350.pt')
 
 # ERROR HANDLERS
 # INSPIRED BY: http://flask.pocoo.org/docs/1.0/patterns/apierrors/
@@ -89,7 +93,7 @@ def index(filename=None):
 def get_sample(filename):
     as_attachment = request.args.get('attachment', None)
     attachment_filename = DEFAULT_SAMPLE_FILENAME if as_attachment else None
-    path_to_file = os.path.join(SAMPLES_FOLDER, filename)
+    path_to_file = SAMPLES_FOLDER / filename
     return send_file(
         path_to_file,
         conditional=True,
@@ -107,18 +111,19 @@ def synthesize():
     if TEXT_ENCODER.decode(TEXT_ENCODER.encode(text)) != text:
         raise ValueError('Text has improper characters.')
 
-    encoded = TEXT_ENCODER.encode(text)
-    encoded = encoded.unsqueeze(1).to(torch.device('cuda'))
-    # predicted_frames [num_frames, batch_size, frame_channels]
-    predicted_frames = feature_model(tokens=encoded)[1]
-    waveform = griffin_lim(predicted_frames[:, 0].cpu().numpy())
+    with torch.set_grad_enabled(False):
+        encoded = TEXT_ENCODER.encode(text)
+        encoded = encoded.unsqueeze(1).to(torch.device('cuda'))
+        # predicted_frames [num_frames, batch_size, frame_channels]
+        predicted_frames = FEATURE_MODEL(tokens=encoded)[1]
+        waveform = griffin_lim(predicted_frames[:, 0].cpu().numpy())
 
     # TODO: Fix this unique_id, it could cause a duplicate
-    unique_id = ''.join(random.choices(string.ascii_uppercase + string.digits, k=10))
-    filename = SAMPLES_FOLDER / '{}{}.mp3'.format('generated_audio_', unique_id)
+    unique_id = ''.join(random.sample(string.ascii_uppercase + string.digits, k=10))
+    filename = SAMPLES_FOLDER / '{}{}.wav'.format('generated_audio_', unique_id)
     librosa.output.write_wav(str(filename), waveform)
 
-    return jsonify({'filename': str(filename)})
+    return jsonify({'filename': str(filename.name)})
 
 
 if __name__ == "__main__":
