46d46bf215ae96ca308183ce6c34054b2a21735e
Michael Petrochuk <petrochukm@gmail.com>
2019-08-20 13:27:35 -0700

    Update sentence splitter to be more efficient

--- src/service/script.js
+++ src/service/script.js
@@ -16,119 +16,136 @@ document.addEventListener('DOMContentLoaded', function (_) {
   const clipsElement = document.querySelector('#clips');
   const splitBySetencesInput = document.querySelector('#split-by-setences');
 
-  async function generate(text) {
+  // `zip` function similar to python.
+  const zip = (a, b) => a.map((x, i) => [x, b[i]]);
+
+  async function generate() {
     /**
      * Generate a clip and append it to the UI.
      */
+    const textSubmitted = textarea.value.trim();
+    const corpus = splitBySetencesInput.checked ? sentenceSplit(textSubmitted) : [textSubmitted];
     const speakerElement = speakerSelect.options[speakerSelect.selectedIndex];
-    const data = {
-      speaker_id: parseInt(speakerElement.value),
-      text: text,
-      api_key: apiKeyInput.value.trim(),
-    }
-    validateParameters(data);
+    const speakerId = parseInt(speakerElement.value);
+    const apiKey = apiKeyInput.value.trim();
+    const payloads = [];
+    const sections = [];
 
-    // Check the stream input is valid
-    const response = await fetch(`${endpoint}/text_to_speech/input_validated`, {
-      method: 'POST',
-      body: JSON.stringify(data),
-      headers: {
-        'Content-Type': 'application/json'
+    for (const text of corpus) {
+      const payload = {
+        speaker_id: speakerId,
+        text: text,
+        api_key: apiKey
+      };
+      payloads.push(payload);
+
+      // Check the stream input is valid
+      validateParameters(payload);
+      const response = await fetch(`${endpoint}/text_to_speech/input_validated`, {
+        method: 'POST',
+        body: JSON.stringify(payload),
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      });
+      if (!response.ok) {
+        throw (await response.json()).message;
       }
-    });
-    if (!response.ok) {
-      throw (await response.json()).message;
+
+      // Create a UI element to recieve stream
+      const sectionElement = document.createElement('section');
+      sectionElement.innerHTML = `<main>
+                                    <div>
+                                      <p><b>${clipNumber}. ${speakerElement.text}</b></p>
+                                      <p>${text}</p>
+                                    </div>
+                                    <div>
+                                      <div class="progress">
+                                        <p>Queuing / Generating Spectrogram</p>
+                                      </div>
+                                      <audio controls></audio>
+                                    </div>
+                                  </main>
+                                  <footer>
+                                    <p></p>
+                                  </footer>`;
+      sections.push(sectionElement);
+      clipsElement.prepend(sectionElement);
+      clipNumber += 1;
     }
 
-    // Create a UI element to recieve stream
-    const sectionElement = document.createElement('section');
-    sectionElement.innerHTML = `<main>
-                                  <div>
-                                    <p><b>${clipNumber}. ${speakerElement.text}</b></p>
-                                    <p>${text}</p>
-                                  </div>
-                                  <div>
-                                    <div class="progress">
-                                      <p>Queuing / Generating Spectrogram</p>
-                                    </div>
-                                    <audio controls></audio>
-                                  </div>
-                                </main>
-                                <footer>
-                                  <p></p>
-                                </footer>`;
-    clipsElement.prepend(sectionElement);
-    clipNumber += 1;
+    const promises = [];
+    for (const [payload, sectionElement] of zip(payloads, sections)) {
+      promises.push(new Promise(function (resolve, _) {
+        // Start stream
+        let startTime = new Date().getTime();
+        let queuingTime;
+        const request = new XMLHttpRequest();
+        let hasProgress = false;
 
-    return new Promise(function (resolve, _) {
-      // TODO: Use the reject parameter and resolve if loadstart does not work.
-      // Start stream
-      let startTime = new Date().getTime();
-      let queuingTime;
-      const request = new XMLHttpRequest();
-      let hasProgress = false;
-
-      // Make request for stream
-      request.open('POST', `${endpoint}/text_to_speech/stream`);
-      request.setRequestHeader('Content-Type', 'application/json');
-      request.responseType = 'blob';
-      request.addEventListener('loadstart', resolve);
-      request.addEventListener('progress', (event) => {
-        if (event.lengthComputable) {
-          if (!hasProgress && event.loaded > 0) {
-            hasProgress = true;
-            // The server sends first WAV headers, before generating the spectrogram.
-            queuingTime = ((new Date().getTime()) - startTime) / 1000;
+        // Make request for stream
+        request.open('POST', `${endpoint}/text_to_speech/stream`);
+        request.setRequestHeader('Content-Type', 'application/json');
+        request.responseType = 'blob';
+        request.addEventListener('loadstart', resolve);
+        request.addEventListener('progress', (event) => {
+          if (event.lengthComputable) {
+            if (!hasProgress && event.loaded > 0) {
+              hasProgress = true;
+              // The server sends first WAV headers, before generating the spectrogram.
+              queuingTime = ((new Date().getTime()) - startTime) / 1000;
+            }
+            let percentage = event.loaded / event.total;
+            const now = new Date().getTime();
+            let estimatedTimeLeft = ((now - startTime) / percentage) * (1 - percentage);
+            estimatedTimeLeft = secondsToString(estimatedTimeLeft / 1000);
+            percentage = Math.round(percentage * 100); // Decimals to percent
+            const message = `${Math.round(percentage)}% Generated - ${estimatedTimeLeft} Left`
+            sectionElement.querySelector('.progress p').textContent = message;
           }
-          let percentage = event.loaded / event.total;
-          const now = new Date().getTime();
-          let estimatedTimeLeft = ((now - startTime) / percentage) * (1 - percentage);
-          estimatedTimeLeft = secondsToString(estimatedTimeLeft / 1000);
-          percentage = Math.round(percentage * 100); // Decimals to percent
-          const message = `${Math.round(percentage)}% Generated - ${estimatedTimeLeft} Left`
-          sectionElement.querySelector('.progress p').textContent = message;
-        }
-      });
-      request.addEventListener('error', (error) => {
-        console.error(error);
-        sectionElement.querySelector('.progress p').textContent = `Network error.`;
-      });
-      request.addEventListener('timeout', () => {
-        sectionElement.querySelector('.progress p').textContent = `Request timed out.`;
-      });
-      request.addEventListener('abort', () => {
-        sectionElement.querySelector('.progress p').textContent = `Request aborted.`;
-      });
-      request.addEventListener('load', () => {
-        if (request.status === 200) {
-          sectionElement.querySelector('.progress').style.display = 'none';
-          const audioElement = sectionElement.querySelector('audio');
-          const generatingTime = ((new Date().getTime()) - startTime) / 1000;
+        });
+        request.addEventListener('error', (error) => {
+          console.error(error);
+          sectionElement.querySelector('.progress p').textContent = `Network error.`;
+        });
+        request.addEventListener('timeout', () => {
+          sectionElement.querySelector('.progress p').textContent = `Request timed out.`;
+        });
+        request.addEventListener('abort', () => {
+          sectionElement.querySelector('.progress p').textContent = `Request aborted.`;
+        });
+        request.addEventListener('load', () => {
+          if (request.status === 200) {
+            sectionElement.querySelector('.progress').style.display = 'none';
+            const audioElement = sectionElement.querySelector('audio');
+            const generatingTime = ((new Date().getTime()) - startTime) / 1000;
 
-          // Add statistics to the footer on `audioElement` load.
-          audioElement.addEventListener('loadedmetadata', () => {
-            // `numSamples` assumes the 16-bit audio and a 44-bit WAV header.
-            const numSamples = (request.getResponseHeader("Content-Length") - 44) / 2;
-            const generatingWaveTime = generatingTime - queuingTime;
-            const samplesPerSecond = Math.round(numSamples / generatingWaveTime);
-            sectionElement.querySelector('footer p').innerHTML = ([
-              `Audio Duration: ${Math.round(audioElement.duration * 100) / 100}s`,
-              `Generation Timer: ${Math.round(generatingTime * 100) / 100}s`,
-              `Queuing / Spectrogram Timer: ${Math.round(queuingTime * 100) / 100}s`,
-              `Waveform Timer: ${Math.round(generatingWaveTime * 100) / 100}s`,
-              `${Math.round(samplesPerSecond)} Samples Per Second`,
-            ].join('&nbsp;&nbsp;|&nbsp;&nbsp;'));
-          });
+            // Add statistics to the footer on `audioElement` load.
+            audioElement.addEventListener('loadedmetadata', () => {
+              // `numSamples` assumes the 16-bit audio and a 44-bit WAV header.
+              const numSamples = (request.getResponseHeader("Content-Length") - 44) / 2;
+              const generatingWaveTime = generatingTime - queuingTime;
+              const samplesPerSecond = Math.round(numSamples / generatingWaveTime);
+              sectionElement.querySelector('footer p').innerHTML = ([
+                `Audio Duration: ${Math.round(audioElement.duration * 100) / 100}s`,
+                `Generation Timer: ${Math.round(generatingTime * 100) / 100}s`,
+                `Queuing / Spectrogram Timer: ${Math.round(queuingTime * 100) / 100}s`,
+                `Waveform Timer: ${Math.round(generatingWaveTime * 100) / 100}s`,
+                `${Math.round(samplesPerSecond)} Samples Per Second`,
+              ].join('&nbsp;&nbsp;|&nbsp;&nbsp;'));
+            });
 
-          audioElement.src = window.URL.createObjectURL(request.response);
-          audioElement.load();
-        } else {
-          const message = `Status code ${request.status}.`;
-          sectionElement.querySelector('.progress p').textContent = message;
-        }
-      });
-      request.send(JSON.stringify(data));
-    });
+            audioElement.src = window.URL.createObjectURL(request.response);
+            audioElement.load();
+          } else {
+            const message = `Status code ${request.status}.`;
+            sectionElement.querySelector('.progress p').textContent = message;
+          }
+        });
+        request.send(JSON.stringify(payload));
+      }));
+    }
+    return Promise.all(promises);
   }
 
   let isGenerateButtonDisabled = false;
@@ -137,17 +154,7 @@ document.addEventListener('DOMContentLoaded', function (_) {
       clearError();
       disableGenerateButton();
       try {
-        const textSubmitted = textarea.value.trim();
-        const promises = [];
-        if (splitBySetencesInput.checked) {
-          const sentences = splitIntoSentences(textSubmitted);
-          for (const sentence of sentences) {
-            promises.push(generate(sentence));
-          }
-        } else {
-          promises.push(generate(textSubmitted));
-        }
-        await Promise.all(promises);
+        await generate();
       } catch (error) {
         displayError(error);
       }
@@ -178,7 +185,7 @@ document.addEventListener('DOMContentLoaded', function (_) {
     }
   }
 
-  function splitIntoSentences(text) {
+  function sentenceSplit(text) {
     /**
      * Inspired by:
      * https://stackoverflow.com/questions/11761563/javascript-regexp-for-splitting-text-into-sentences-and-keeping-the-delimiter
