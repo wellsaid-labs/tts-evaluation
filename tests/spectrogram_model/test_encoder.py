17b4d6c4e2baf8ac9b72b859ea5a077480bc6c09
Michael Petrochuk <petrochukm@gmail.com>
2019-12-16 18:58:43 -0800

    Add speaker embedding

--- tests/spectrogram_model/test_encoder.py
+++ tests/spectrogram_model/test_encoder.py
@@ -3,13 +3,11 @@ import torch
 from src.spectrogram_model.encoder import Encoder
 
 encoder_params = {
-    'num_speakers': 2,
     'batch_size': 4,
     'num_tokens': 5,
     'vocab_size': 10,
     'lstm_hidden_size': 64,
     'token_embedding_dim': 32,
-    'speaker_embedding_dim': 16,
     'lstm_bidirectional': True,
     'out_dim': 8,
 }
@@ -18,44 +16,16 @@ encoder_params = {
 def test_encoder():
     encoder = Encoder(
         encoder_params['vocab_size'],
-        encoder_params['num_speakers'],
         out_dim=encoder_params['out_dim'],
         lstm_hidden_size=encoder_params['lstm_hidden_size'],
         lstm_bidirectional=encoder_params['lstm_bidirectional'],
-        speaker_embedding_dim=encoder_params['speaker_embedding_dim'],
         token_embedding_dim=encoder_params['token_embedding_dim'])
 
     # NOTE: 1-index to avoid using 0 typically associated with padding
     input_ = torch.LongTensor(encoder_params['batch_size'],
                               encoder_params['num_tokens']).random_(1, encoder_params['vocab_size'])
-    speaker = torch.LongTensor(encoder_params['batch_size']).fill_(0)
     tokens_mask = torch.full((encoder_params['batch_size'], encoder_params['num_tokens']), 1).bool()
-    output = encoder(input_, tokens_mask, speaker)
-
-    assert output.type() == 'torch.FloatTensor'
-    assert output.shape == (encoder_params['num_tokens'], encoder_params['batch_size'],
-                            encoder_params['out_dim'])
-
-    # Smoke test backward
-    output.sum().backward()
-
-
-def test_encoder_one_speaker():
-    encoder = Encoder(
-        encoder_params['vocab_size'],
-        1,
-        out_dim=encoder_params['out_dim'],
-        lstm_hidden_size=encoder_params['lstm_hidden_size'],
-        lstm_bidirectional=encoder_params['lstm_bidirectional'],
-        speaker_embedding_dim=encoder_params['speaker_embedding_dim'],
-        token_embedding_dim=encoder_params['token_embedding_dim'])
-
-    # NOTE: 1-index to avoid using 0 typically associated with padding
-    input_ = torch.LongTensor(encoder_params['batch_size'],
-                              encoder_params['num_tokens']).random_(1, encoder_params['vocab_size'])
-    speaker = torch.LongTensor(encoder_params['batch_size']).fill_(0)
-    tokens_mask = torch.full((encoder_params['batch_size'], encoder_params['num_tokens']), 1).bool()
-    output = encoder(input_, tokens_mask, speaker)
+    output = encoder(input_, tokens_mask)
 
     assert output.type() == 'torch.FloatTensor'
     assert output.shape == (encoder_params['num_tokens'], encoder_params['batch_size'],
@@ -69,11 +39,9 @@ def test_encoder_filter_size():
     for filter_size in [1, 3, 5]:
         encoder = Encoder(
             encoder_params['vocab_size'],
-            encoder_params['num_speakers'],
             out_dim=encoder_params['out_dim'],
             lstm_hidden_size=encoder_params['lstm_hidden_size'],
             lstm_bidirectional=encoder_params['lstm_bidirectional'],
-            speaker_embedding_dim=encoder_params['speaker_embedding_dim'],
             token_embedding_dim=encoder_params['token_embedding_dim'],
             convolution_filter_size=filter_size)
 
@@ -81,10 +49,9 @@ def test_encoder_filter_size():
         input_ = torch.LongTensor(encoder_params['batch_size'],
                                   encoder_params['num_tokens']).random_(
                                       1, encoder_params['vocab_size'])
-        speaker = torch.LongTensor(encoder_params['batch_size']).fill_(0)
         tokens_mask = torch.full((encoder_params['batch_size'], encoder_params['num_tokens']),
                                  1).bool()
-        output = encoder(input_, tokens_mask, speaker)
+        output = encoder(input_, tokens_mask)
 
         assert output.type() == 'torch.FloatTensor'
         assert output.shape == (encoder_params['num_tokens'], encoder_params['batch_size'],
