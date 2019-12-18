a9b326aff0789023e8459224d37aad5962e2adef
Michael Petrochuk <petrochukm@gmail.com>
2019-12-17 20:41:02 -0800

    Add tag

--- src/bin/train/spectrogram_model/__main__.py
+++ src/bin/train/spectrogram_model/__main__.py
@@ -252,7 +252,7 @@ if __name__ == '__main__':  # pragma: no cover
         '--tags',
         default=[
             'detached post_net', 'masked conv', 'post_net dropout=0', 'weight_decay=10**-6',
-            'no numbers', 'decoder speaker embedding net'
+            'no numbers', 'decoder speaker embedding net', 'masked backward lstm'
         ],
         nargs='+',
         help='List of tags for a new experiments.')
