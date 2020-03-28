590fb67ef13a9d75290ed1fc82f7aaa69d6206d9
Michael Petrochuk <petrochukm@gmail.com>
2020-03-27 19:51:42 -0700

    Add distributed data parallel to discriminator

--- src/bin/train/signal_model/__main__.py
+++ src/bin/train/signal_model/__main__.py
@@ -266,7 +266,7 @@ if __name__ == '__main__':  # pragma: no cover
         '--tags',
         default=[
             'weight_norm_conditional', 'power_2_windows', 'layer_norm_input',
-            '100x_discriminator_lr'
+            '100x_discriminator_lr', 'synchronize_discriminator'
         ],
         nargs='+',
         help='List of tags for the experiment.')
