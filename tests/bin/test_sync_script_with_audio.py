50c1a5cb50567d3c1eb71ad210ef423c061c5e7d
Michael Petrochuk <petrochukm@gmail.com>
2020-08-08 11:24:29 -0700

    Fix tests

--- tests/bin/test_sync_script_with_audio.py
+++ tests/bin/test_sync_script_with_audio.py
@@ -2,5 +2,5 @@ from src.bin.sync_script_with_audio import _get_speech_context_phrases
 
 
 def test___get_speech_context_phrases():
-    assert _get_speech_context_phrases(['a b c d e f g h i j'],
-                                       5) == ['a b c', 'd e f', 'g h i', 'j']
+    assert set(_get_speech_context_phrases(['a b c d e f g h i j'],
+                                           5).phrases) == set(['a b c', 'd e f', 'g h i', 'j'])
