cc4dc5ab9b339cd5ff6f22195c4b09048da64024
Michael Petrochuk <petrochukm@gmail.com>
2020-10-19 10:23:18 -0700

    Update formatting

--- lib/datasets/__init__.py
+++ lib/datasets/__init__.py
@@ -1,55 +1,59 @@
-from lib.datasets.lj_speech import LINDA_JOHNSON
-from lib.datasets.lj_speech import lj_speech_dataset
-from lib.datasets.m_ailabs import ELIZABETH_KLETT
-from lib.datasets.m_ailabs import ELLIOT_MILLER
-from lib.datasets.m_ailabs import JUDY_BIEBER
-from lib.datasets.m_ailabs import m_ailabs_en_uk_elizabeth_klett_speech_dataset
-from lib.datasets.m_ailabs import m_ailabs_en_us_elliot_miller_speech_dataset
-from lib.datasets.m_ailabs import m_ailabs_en_us_judy_bieber_speech_dataset
-from lib.datasets.m_ailabs import m_ailabs_en_us_mary_ann_speech_dataset
-from lib.datasets.m_ailabs import MARY_ANN
-from lib.datasets.utils import Alignment
-from lib.datasets.utils import dataset_generator
-from lib.datasets.utils import dataset_loader
-from lib.datasets.utils import Example
-from lib.datasets.utils import precut_dataset_loader
-from lib.datasets.utils import Speaker
+from lib.datasets.lj_speech import LINDA_JOHNSON, lj_speech_dataset
+from lib.datasets.m_ailabs import (
+    ELIZABETH_KLETT,
+    ELLIOT_MILLER,
+    JUDY_BIEBER,
+    MARY_ANN,
+    m_ailabs_en_uk_elizabeth_klett_speech_dataset,
+    m_ailabs_en_us_elliot_miller_speech_dataset,
+    m_ailabs_en_us_judy_bieber_speech_dataset,
+    m_ailabs_en_us_mary_ann_speech_dataset,
+)
+from lib.datasets.utils import (
+    Alignment,
+    Example,
+    Speaker,
+    dataset_generator,
+    dataset_loader,
+    precut_dataset_loader,
+)
 
 # TODO: Consider not using the actors realnames in the codebase in an effort to protect their
 # privacy.
 
-HILARY_NORIEGA = Speaker('Hilary Noriega')
+HILARY_NORIEGA = Speaker("Hilary Noriega")
+ALICIA_HARRIS = Speaker("Alicia Harris")
+MARK_ATHERLAY = Speaker("Mark Atherlay")
+SAM_SCHOLL = Speaker("Sam Scholl")
 
 
-def hilary_noriega_speech_dataset(root_directory_name: str = 'hilary_noriega',
-                                  gcs_path: str = 'gs://wellsaid_labs_datasets/hilary_noriega',
-                                  speaker: Speaker = HILARY_NORIEGA,
-                                  **kwargs):
-    return dataset_loader(root_directory_name, gcs_path, speaker, **kwargs)
+def hilary_noriega_speech_dataset(speaker: Speaker = HILARY_NORIEGA, **kwargs):
+    return _dataset_loader(speaker, **kwargs)
 
 
-ALICIA_HARRIS = Speaker('Alicia Harris')
-MARK_ATHERLAY = Speaker('Mark Atherlay')
-SAM_SCHOLL = Speaker('Sam Scholl')
+def _dataset_loader(speaker: Speaker, gcs_path: str = "gs://wellsaid_labs_datasets/", **kwargs):
+    label = HILARY_NORIEGA.name.lower().replace(" ", "_")
+    return dataset_loader(label, gcs_path + label, speaker, **kwargs)
+
 
 __all__ = [
-    'Speaker',
-    'Example',
-    'Alignment',
-    'dataset_generator',
-    'dataset_loader',
-    'precut_dataset_loader',
-    'lj_speech_dataset',
-    'm_ailabs_en_us_judy_bieber_speech_dataset',
-    'm_ailabs_en_us_mary_ann_speech_dataset',
-    'm_ailabs_en_us_elliot_miller_speech_dataset',
-    'm_ailabs_en_uk_elizabeth_klett_speech_dataset',
-    'ALICIA_HARRIS',
-    'ELIZABETH_KLETT',
-    'ELLIOT_MILLER',
-    'JUDY_BIEBER',
-    'LINDA_JOHNSON',
-    'MARK_ATHERLAY',
-    'MARY_ANN',
-    'SAM_SCHOLL',
+    "Speaker",
+    "Example",
+    "Alignment",
+    "dataset_generator",
+    "dataset_loader",
+    "precut_dataset_loader",
+    "lj_speech_dataset",
+    "m_ailabs_en_us_judy_bieber_speech_dataset",
+    "m_ailabs_en_us_mary_ann_speech_dataset",
+    "m_ailabs_en_us_elliot_miller_speech_dataset",
+    "m_ailabs_en_uk_elizabeth_klett_speech_dataset",
+    "ALICIA_HARRIS",
+    "ELIZABETH_KLETT",
+    "ELLIOT_MILLER",
+    "JUDY_BIEBER",
+    "LINDA_JOHNSON",
+    "MARK_ATHERLAY",
+    "MARY_ANN",
+    "SAM_SCHOLL",
 ]
