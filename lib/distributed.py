+ Remove Average metric + Fix clipper so the `norm` is computed before `clip_grad_norm_` is run + Fix type errors in distributed + Add `gather`, `reduce`, `all_gather` and `gather_list` for syncing measurements + Ensure `spawn` can be used on cpu + Add notes + Update outdated documentation + Remove mock from distributed tests

lib/distributed.py
@@ -7,9 +7,11 @@ import typing
 
 import torch
 
-import lib
-
 logger = logging.getLogger(__name__)
+
+
+# TODO: Rename `master` to `main`, learn more:
+# https://www.wired.com/story/tech-confronts-use-labels-master-slave/
 
 
 def is_initialized() -> bool:
@@ -29,31 +31,75 @@ def is_master() -> bool:
     return torch.distributed.get_rank() == get_master_rank()
 
 
-def assert_synced(value: float, message: str = ""):
+_default_device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+
+def assert_synced(value: float, message: str = "", device=_default_device):
     """Assert that `value` is the same between master and worker nodes.
 
     NOTE: The `value` is split into digits to support large numbers like 128-bit hashes.
+    TODO: Factor out a utility function to `torch.distributed.broadcast` 128-bit bit numbers.
 
     Args:
         value: Value to check.
         message: Assert message.
     """
-    torch_ = torch.cuda if torch.cuda.is_available() else torch
     if is_master():
-        length = torch_.LongTensor([len(str(value))])  # type: ignore
+        length = torch.tensor([len(str(value))], dtype=torch.long, device=device)
     else:
-        length = torch_.LongTensor(1)  # type: ignore
+        length = torch.zeros(1, dtype=torch.long, device=device)
     torch.distributed.broadcast(length, src=get_master_rank())
-    length = length.item()
-    assert len(str(value)) == length, message
-    value_tensor = torch_.LongTensor([int(d) for d in str(value)])  # type: ignore
+    length_ = int(length.item())
+    assert len(str(value)) == length_, message
+    value_tensor = torch.tensor([int(d) for d in str(value)], dtype=torch.long, device=device)
     if is_master():
         torch.distributed.broadcast(value_tensor, src=get_master_rank())
         master_value = value_tensor
     else:
-        master_value = torch_.LongTensor(length)  # type: ignore
+        master_value = torch.zeros(length_, dtype=torch.long, device=device)
         torch.distributed.broadcast(master_value, src=get_master_rank())
     assert torch.equal(master_value, value_tensor), message
+
+
+def reduce_(value: float, dst: int = get_master_rank(), device=_default_device, **kwargs) -> float:
+    """Reduce `value` from all processes via a reduction operation
+    like `torch.distributed.ReduceOp.SUM`."""
+    packed = torch.tensor([value], dtype=torch.float, device=device)
+    torch.distributed.reduce(packed, dst=dst, **kwargs)
+    return packed.item()
+
+
+def gather(
+    value: float, dst: int = get_master_rank(), device=_default_device, **kwargs
+) -> typing.List[float]:
+    """ Gather `value` from all processes into a `list` on the `dst` process. """
+    world_size = torch.distributed.get_world_size()
+    return_ = [torch.zeros(1, device=device, dtype=torch.float) for _ in range(world_size)]
+    tensor = torch.tensor([value], device=device, dtype=torch.float)
+    torch.distributed.gather(tensor, return_ if is_master() else None, dst=dst, **kwargs)
+    return [t.item() for t in return_]
+
+
+def all_gather(value: float, device=_default_device, **kwargs) -> typing.List[float]:
+    """ Gather `value` from all processes into a `list`. """
+    world_size = torch.distributed.get_world_size()
+    return_ = [torch.zeros(1, device=device, dtype=torch.float) for _ in range(world_size)]
+    tensor = torch.tensor([value], device=device, dtype=torch.float)
+    torch.distributed.all_gather(return_, tensor, **kwargs)
+    return [t.item() for t in return_]
+
+
+def gather_list(
+    values: typing.List[float], dst: int = get_master_rank(), device=_default_device, **kwargs
+) -> typing.List[typing.List[float]]:
+    """ Gather `values` from all processes into a `list` on the `dst` process. """
+    lengths = [int(l) for l in all_gather(len(values), device=device, **kwargs)]
+    max_ = max(lengths)
+    return_ = [torch.zeros(max_, device=device, dtype=torch.float) for _ in lengths]
+    tensor = torch.tensor(values, device=device, dtype=torch.float)
+    tensor = torch.nn.functional.pad(tensor, [0, max_ - len(values)])
+    torch.distributed.gather(tensor, return_ if is_master() else None, dst=dst, **kwargs)
+    return [t.tolist()[:l] for t, l in zip(return_, lengths)]
 
 
 def spawn(*args, nprocs=None, **kwargs):
@@ -62,38 +108,7 @@ def spawn(*args, nprocs=None, **kwargs):
     NOTE (michael): Without an assert, when `nprocs` is zero, `torch.multiprocessing.spawn`
     crashes in a nondescript way.
     """
-    assert torch.cuda.device_count() > 0, "Unable to find CUDA devices."
-    nprocs = torch.cuda.device_count() if nprocs is None else nprocs
-    torch.multiprocessing.spawn(*args, nprocs=nprocs, **kwargs)  # type: ignore
-
-
-class DistributedAverage(lib.utils.Average):
-    """ Track the average in a distributed environment. """
-
-    def reset(self) -> typing.Optional[float]:
-        super().reset()
-        average = (
-            self.post_sync_total_value / self.post_sync_total_count
-            if (
-                hasattr(self, "post_sync_total_value")
-                and hasattr(self, "post_sync_total_value")
-                and self.post_sync_total_count > 0
-            )
-            else None
-        )
-        self.post_sync_total_value: float = 0.0
-        self.post_sync_total_count: float = 0.0
-        return average
-
-    def sync(self) -> DistributedAverage:
-        """ Synchronize measurements accross multiple processes. """
-        last_post_sync_total_value = self.post_sync_total_value
-        last_post_sync_total_count = self.post_sync_total_count
-        torch_ = torch.cuda if torch.cuda.is_available() else torch
-        packed = torch_.FloatTensor([self.total_value, self.total_count])  # type: ignore
-        torch.distributed.reduce(packed, dst=get_master_rank())
-        self.post_sync_total_value, self.post_sync_total_count = tuple(packed.tolist())
-        self.last_update_value = (self.post_sync_total_value - last_post_sync_total_value) / (
-            self.post_sync_total_count - last_post_sync_total_count
-        )
-        return self
+    if torch.cuda.is_available():
+        assert torch.cuda.device_count() > 0, "Unable to find CUDA devices."
+        nprocs = torch.cuda.device_count() if nprocs is None else nprocs
+    return torch.multiprocessing.spawn(*args, nprocs=nprocs, **kwargs)  # type: ignore

lib/optimizers.py
@@ -75,13 +75,13 @@ class AdaptiveGradientNormClipper:
     def clip(self):
         """Clips gradient norm of an iterable of `self.parameters`, and update gradient norm
         history."""
+        norm = get_parameter_norm(self.parameters, self.norm_type)
+        if not np.isfinite(norm):
+            raise ValueError(f"Gradient is not finite: {norm}")
         if self.max_norm is not None:
             torch.nn.utils.clip_grad_norm_(
                 self.parameters, max_norm=self.max_norm, norm_type=self.norm_type
             )
-        norm = get_parameter_norm(self.parameters, self.norm_type)
-        if not np.isfinite(norm):
-            raise ValueError(f"Gradient is not finite: {norm}")
         self._insert(norm)
         self.max_norm = self._get_median()
 

lib/spectrogram_model/model.py
@@ -132,7 +132,7 @@ class SpectrogramModel(nn.Module):
             split_size
             num_tokens (torch.LongTensor [batch_size])
             tokens_mask (torch.BoolTensor [batch_size, num_tokens])
-            speaker (torch.LongTensor [batch_size, speaker_embedding_dim])
+            speaker (torch.LongTensor [batch_size, speaker_embedding_size])
             use_tqdm: Add a progress bar for non-batch generation.
 
         Returns:
@@ -290,7 +290,7 @@ class SpectrogramModel(nn.Module):
         # [num_frames, batch_size]
         frames_mask = lengths_to_mask(target_lengths, device=target_frames.device).transpose(0, 1)
 
-        speaker = self.embed_speaker(speaker)  # [batch_size] → [batch_size, speaker_embedding_dim]
+        speaker = self.embed_speaker(speaker)  # [batch_size] → [batch_size, speaker_embedding_size]
         # [batch_size, num_tokens] → [num_tokens, batch_size, encoder_hidden_size]
         encoded_tokens = self.encoder(tokens, tokens_mask, num_tokens, speaker)
         frames, stop_tokens, alignments, hidden_state = self.decoder(
@@ -343,7 +343,7 @@ class SpectrogramModel(nn.Module):
         tokens, num_tokens, speaker = self._normalize_inputs(tokens, speaker, num_tokens)
         tokens_mask = lengths_to_mask(num_tokens, device=tokens.device)  # [batch_size, num_tokens]
 
-        speaker = self.embed_speaker(speaker)  # [batch_size] → [batch_size, speaker_embedding_dim]
+        speaker = self.embed_speaker(speaker)  # [batch_size] → [batch_size, speaker_embedding_size]
         # [batch_size, num_tokens] → [num_tokens, batch_size, encoder_hidden_size]
         encoded_tokens = self.encoder(tokens, tokens_mask, num_tokens, speaker)
         generator = self._infer_generator(

tests/lib/test_distributed.py
@@ -1,4 +1,4 @@
-from unittest import mock
+import functools
 
 import pytest
 import torch
@@ -6,81 +6,147 @@ import torch
 import lib
 
 
-def test_is_master__not_initialized():
-    """Test `lib.distributed.is_master` and `lib.distributed.is_initialized`  base case on a
-    machine without CUDA.
-    """
-    assert not torch.cuda.is_available()
+def init_process_group(rank, nprocs, backend="gloo", init_method="tcp://127.0.0.1:23456"):
+    torch.distributed.init_process_group(
+        backend=backend, init_method=init_method, world_size=nprocs, rank=rank
+    )
+
+
+def _is_initialized(rank, nprocs):
+    """ Helper function for `test_is_initialized*`. """
+    init_process_group(rank=rank, nprocs=nprocs)
+    assert lib.distributed.is_initialized()
+
+
+def test_is_initialized():
+    """ Test `lib.distributed.is_initialized` returns `True` if distributed is initialized."""
+    nprocs = 2
+    torch.multiprocessing.spawn(functools.partial(_is_initialized, nprocs=nprocs), nprocs=nprocs)
+
+
+def test_is_initialized__not_initialized():
+    """ Test `lib.distributed.is_initialized` returns `False` if distributed isn't initialized."""
     assert not lib.distributed.is_initialized()
+
+
+def _is_master(rank, nprocs):
+    """ Helper function for `test_is_master`. """
+    init_process_group(rank=rank, nprocs=nprocs)
+    assert lib.distributed.is_master() == (rank == lib.distributed.get_master_rank())
+
+
+def test_is_master():
+    """ Test `lib.distributed.is_master` differentiates master and worker processes. """
+    nprocs = 2
+    torch.multiprocessing.spawn(functools.partial(_is_master, nprocs=nprocs), nprocs=nprocs)
+
+
+def test_is_master__not_initialized():
+    """ Test `lib.distributed.is_master` returns `True` if distributed is initialized. """
     assert lib.distributed.is_master()
 
 
-@mock.patch("lib.distributed.is_initialized", return_value=True)
-@mock.patch("lib.distributed.torch.distributed")
-def test_is_master(mock_distributed, _):
-    """ Test `lib.distributed.is_master` if distributed is initialized. """
-    mock_distributed.get_rank.return_value = lib.distributed.get_master_rank()
-    assert lib.distributed.is_master()
+def _assert_synced(rank, nprocs, master_value, worker_value):
+    """ Helper function for `test_assert_synced*`. """
+    init_process_group(rank=rank, nprocs=nprocs)
+    value = master_value if lib.distributed.is_master() else worker_value
+    lib.distributed.assert_synced(value)
 
 
-@mock.patch("lib.distributed.is_master", return_value=True)
-@mock.patch("lib.distributed.torch.distributed")
-def test_assert_synced__master(mock_distributed, _):
-    """ Test `lib.distributed.assert_synced` passes for master process. """
-    mock_distributed.broadcast.return_value = None
-    lib.distributed.assert_synced(123, "")
+def test_assert_synced():
+    """ Test `lib.distributed.assert_synced` doesn't error if the values do match. """
+    nprocs = 2
+    partial = functools.partial(_assert_synced, nprocs=nprocs, master_value=1, worker_value=1)
+    torch.multiprocessing.spawn(partial, nprocs=nprocs)
 
 
-def _mock_broadcast_side_effect(tensor, src):
-    """ Mock broadcast of `length` 3 and value `123`. """
-    if tensor.shape[0] == 1:
-        tensor.copy_(torch.tensor([3]))
-    else:
-        tensor.copy_(torch.tensor([1, 2, 3]))
+def test_assert_synced__multiple_digits():
+    """Test `lib.distributed.assert_synced` doesn't error if the values do match and have
+    multiple digits."""
+    nprocs = 2
+    partial = functools.partial(_assert_synced, nprocs=nprocs, master_value=123, worker_value=123)
+    torch.multiprocessing.spawn(partial, nprocs=nprocs)
 
 
-@mock.patch("lib.distributed.is_master", return_value=False)
-@mock.patch("lib.distributed.torch.distributed")
-def test_assert_synced__worker(mock_distributed, _):
-    """ Test `lib.distributed.assert_synced` passes for worker process with the correct value. """
-    mock_distributed.broadcast.side_effect = _mock_broadcast_side_effect
-    lib.distributed.assert_synced(123, "")
+def test_assert_synced__no_match():
+    """ Test `lib.distributed.assert_synced` errors if the values don't match. """
+    nprocs = 2
+    partial = functools.partial(_assert_synced, nprocs=nprocs, master_value=1, worker_value=2)
+    with pytest.raises(Exception):
+        torch.multiprocessing.spawn(partial, nprocs=nprocs)
 
 
-@mock.patch("lib.distributed.is_master", return_value=False)
-@mock.patch("lib.distributed.torch.distributed")
-def test_assert_synced__worker__wrong_value(mock_distributed, _):
-    """ Test `lib.distributed.assert_synced` failes for worker process with the wrong value. """
-    mock_distributed.broadcast.side_effect = _mock_broadcast_side_effect
-    with pytest.raises(AssertionError):
-        lib.distributed.assert_synced(124)
+def test_assert_synced__no_match__wrong_length():
+    """ Test `lib.distributed.assert_synced` errors if the values have different lengths. """
+    nprocs = 2
+    partial = functools.partial(_assert_synced, nprocs=nprocs, master_value=12, worker_value=123)
+    with pytest.raises(Exception):
+        torch.multiprocessing.spawn(partial, nprocs=nprocs)
 
 
-@mock.patch("lib.distributed.is_master", return_value=False)
-@mock.patch("lib.distributed.torch.distributed")
-def test_assert_synced__worker__wrong_length(mock_distributed, _):
-    """ Test `lib.distributed.assert_synced` failes for worker process with the wrong value. """
-    mock_distributed.broadcast.side_effect = _mock_broadcast_side_effect
-    with pytest.raises(AssertionError):
-        lib.distributed.assert_synced(12)
+def test_assert_synced__single_process():
+    """ Test `lib.distributed.assert_synced` doesn't error if there is only a single process. """
+    nprocs = 1
+    partial = functools.partial(_assert_synced, nprocs=nprocs, master_value=12, worker_value=123)
+    torch.multiprocessing.spawn(partial, nprocs=nprocs)
 
 
-def test_spawn():
-    """ Test `lib.distributed.spawn` base case on a machine without CUDA. """
-    assert not torch.cuda.is_available()
-    with pytest.raises(AssertionError):
-        lib.distributed.spawn()
+def assert_(
+    rank,
+    nprocs,
+    callable_,
+    expected,
+    backend="gloo",
+    init_method="tcp://127.0.0.1:23456",
+    test_workers=False,
+):
+    """ `assert_` the result of a distributed operation. """
+    init_process_group(rank=rank, nprocs=nprocs)
+    return_ = callable_(rank)
+    if test_workers or lib.distributed.is_master():
+        assert return_ == expected
 
 
-@mock.patch("torch.distributed")
-def test_distributed_average_metric(mock_distributed):
-    """ Test `DistributedAverage` is able to track the average afer a `sync`. """
-    mock_distributed.reduce.return_value = None
-    metric = lib.distributed.DistributedAverage()
-    assert metric.last_update_value is None
-    metric.update(torch.tensor([0.5]), torch.tensor(3))
-    metric.update(0.25, 2)
-    assert metric.last_update_value == 0.25
-    assert metric.sync().last_update_value == 0.4
-    assert metric.reset() == 0.4
-    assert metric.last_update_value is None
+def test_reduce():
+    """ Test `lib.distributed.reduce_` reduces values from work processes to the main process. """
+    nprocs = 3
+    partial = functools.partial(
+        assert_, nprocs=nprocs, callable_=lib.distributed.reduce_, expected=3
+    )
+    torch.multiprocessing.spawn(partial, nprocs=nprocs)
+
+
+def test_gather():
+    """ Test `lib.distributed.gather` gathers values from work processes to the main process. """
+    nprocs = 2
+    partial = functools.partial(
+        assert_, nprocs=nprocs, callable_=lib.distributed.gather, expected=list(range(nprocs))
+    )
+    torch.multiprocessing.spawn(partial, nprocs=nprocs)
+
+
+def test_all_gather():
+    """ Test `lib.distributed.all_gather` gathers values from work processes. """
+    nprocs = 2
+    partial = functools.partial(
+        assert_,
+        nprocs=nprocs,
+        callable_=lib.distributed.all_gather,
+        expected=list(range(nprocs)),
+        test_workers=True,
+    )
+    torch.multiprocessing.spawn(partial, nprocs=nprocs)
+
+
+def _gather_list(rank):
+    """ Helper function for `test_gather_list`. """
+    return lib.distributed.gather_list([rank] * rank)
+
+
+def test_gather_list():
+    """ Test `lib.distributed.gather_list` gathers list from work processes to the main process. """
+    nprocs = 3
+    partial = functools.partial(
+        assert_, nprocs=nprocs, callable_=_gather_list, expected=[[], [1], [2, 2]]
+    )
+    torch.multiprocessing.spawn(partial, nprocs=nprocs)