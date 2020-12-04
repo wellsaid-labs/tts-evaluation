4fdf83ae22c5afab6e0cc57a75ebfcd9d064a257
Michael Petrochuk <petrochukm@gmail.com>
2020-12-04 10:48:10 -0800

    Fix and test Python 3.8 installation

--- run/utils/apt_install.sh
+++ run/utils/apt_install.sh
@@ -1,8 +1,12 @@
-# Install Python 3.8
+sudo add-apt-repository ppa:deadsnakes/ppa -y
+sudo apt update -y
+
+# Install Python 3.8. Learn more about this approach:
+# https://linuxconfig.org/how-to-change-from-default-to-alternative-python-version-on-debian-linux
 sudo apt install python3.8 -y
-sudo rm /usr/bin/python3
-sudo ln -s python3.8 /usr/bin/python3
-python3 -V
+sudo update-alternatives --install /usr/bin/python3 python3 $(readlink -f /usr/bin/python3) 1
+sudo update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.8 2
+sudo update-alternatives --set python3 /usr/bin/python3.8
 sudo apt-get install python3.8-venv python3.8-dev -y
 
 # Install other dependencies
