+ Add unit conversion metrics for audio + Remove @configurable from basic audio unit conversion + Add related test + Fix config errors

lib/audio.py
@@ -34,6 +34,36 @@ else:
 logger = logging.getLogger(__name__)
 
 
+def milli_to_sec(milli: float) -> float:
+    """ Covert milliseconds to seconds. """
+    return milli / 1000
+
+
+def sec_to_milli(sec: float) -> float:
+    """ Covert seconds to milliseconds. """
+    return sec * 1000
+
+
+def milli_to_sample(milli: float, sample_rate: int) -> int:
+    """ Covert milliseconds to samples. """
+    return int(round(milli_to_sec(milli) * sample_rate))
+
+
+def sample_to_milli(sample: int, sample_rate: int) -> float:
+    """ Covert samples to milliseconds. """
+    return sec_to_milli(float(sample) / sample_rate)
+
+
+def sample_to_sec(sample: int, sample_rate: int) -> float:
+    """ Covert samples to seconds. """
+    return float(sample) / sample_rate
+
+
+def sec_to_sample(sec: float, sample_rate: int) -> int:
+    """ Covert seconds to samples. """
+    return int(round(sec * sample_rate))
+
+
 class AudioEncoding(enum.Enum):
     MPEG: typing.Final = "MPEG audio (layer I, II or III)"
     PCM_INT_8_BIT: typing.Final = "8-bit Signed Integer PCM"
@@ -71,7 +101,7 @@ class AudioMetadata(typing.NamedTuple):
 
     @property
     def length(self):
-        return self.num_samples / self.sample_rate
+        return sample_to_sec(self.num_samples, self.sample_rate)
 
 
 def _parse_audio_metadata(metadata: str) -> AudioMetadata:
@@ -166,16 +196,6 @@ def get_audio_metadata(paths, **kwargs):
     is_list = isinstance(paths, list)
     metadatas = list(_get_audio_metadata(*tuple(paths if is_list else [paths]), **kwargs))
     return metadatas if is_list else metadatas[0]
-
-
-@configurable
-def seconds_to_samples(seconds: float, sample_rate: int = HParam()) -> int:
-    return int(round(seconds * sample_rate))
-
-
-@configurable
-def samples_to_seconds(samples: int, sample_rate: int = HParam()) -> float:
-    return float(samples) / sample_rate
 
 
 def clip_waveform(waveform: np.ndarray) -> np.ndarray:
@@ -963,7 +983,7 @@ def get_signal_to_db_mel_spectrogram(*args, **kwargs) -> SignalTodBMelSpectrogra
 
 @configurable
 def get_pyloudnorm_meter(
-    sample_rate: int = HParam(), filter_class: str = HParam(), **kwargs
+    sample_rate: int, filter_class: str = HParam(), **kwargs
 ) -> pyloudnorm.Meter:
     return _get_pyloudnorm_meter(sample_rate=sample_rate, filter_class=filter_class, **kwargs)
 

run/train/spectrogram_model/_data.py
@@ -28,7 +28,7 @@ from torchnlp.utils import lengths_to_mask
 
 import lib
 import run
-from lib.audio import seconds_to_samples
+from lib.audio import sec_to_sample
 from lib.distributed import get_rank, get_world_size, is_initialized
 from lib.samplers import BucketBatchSampler
 from lib.utils import Tuple, flatten_2d
@@ -178,10 +178,10 @@ def _random_nonoverlapping_alignments(
 @configurable
 def _get_loudness(
     audio: numpy.ndarray,
+    sample_rate: int,
     alignment: Alignment,
     block_size: float = HParam(),
     precision: int = HParam(),
-    **kwargs,
 ) -> typing.Optional[float]:
     """Get the loudness in LUFS for an `alignment` in `audio`.
 
@@ -193,19 +193,17 @@ def _get_loudness(
         precision: The number of decimal places to round LUFS.
         ...
     """
-    meter = lib.audio.get_pyloudnorm_meter(block_size=block_size, **kwargs)
-    if "sample_rate" in kwargs:
-        kwargs = {"sample_rate": kwargs["sample_rate"]}
-    _to_samples = functools.partial(seconds_to_samples, **kwargs)
-    slice_ = audio[_to_samples(alignment.audio[0]) : _to_samples(alignment.audio[1])]
-    if slice_.shape[0] >= _to_samples(block_size):
+    meter = lib.audio.get_pyloudnorm_meter(sample_rate, block_size=block_size)
+    sec_to_sample_ = functools.partial(sec_to_sample, sample_rate=sample_rate)
+    slice_ = audio[sec_to_sample_(alignment.audio[0]) : sec_to_sample_(alignment.audio[1])]
+    if slice_.shape[0] >= sec_to_sample_(block_size):
         return round(meter.integrated_loudness(slice_), precision)
     return None
 
 
 @configurable
 def _random_loudness_annotations(
-    span: Span, signal: numpy.ndarray, max_annotations: int = HParam(), **kwargs
+    span: Span, signal: numpy.ndarray, max_annotations: int = HParam()
 ) -> typing.Tuple[torch.Tensor, torch.Tensor]:
     """
     Args:
@@ -216,7 +214,7 @@ def _random_loudness_annotations(
     loudness_mask = torch.zeros(len(span.script), dtype=torch.bool)
     for alignment in _random_nonoverlapping_alignments(span.alignments, max_annotations):
         slice_ = slice(alignment.script[0], alignment.script[1])
-        loudness_ = _get_loudness(signal, alignment, **kwargs)
+        loudness_ = _get_loudness(signal, span.audio_file.sample_rate, alignment)
         if loudness_ is not None:
             loudness[slice_] = loudness_
             loudness_mask[slice_] = True