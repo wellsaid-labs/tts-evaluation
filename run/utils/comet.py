33be1799f4f6b1fbbd96cc5d43284b417d13d2d1
Michael Petrochuk <petrochukm@gmail.com>
2021-04-16 14:00:15 -0700

    Add functionality to download all samples from Comet

--- run/utils/comet.py
+++ run/utils/comet.py
@@ -1,9 +1,14 @@
 """ Command-line interface (CLI) for Comet. """
 import math
+import pathlib
 import subprocess
 
 import comet_ml
+import tqdm
 import typer
+
+import lib
+import run
 
 app = typer.Typer(context_settings=dict(max_content_width=math.inf))
 
@@ -25,8 +30,24 @@ def patch(experiment: str):
     """ Apply git patch created for a EXPERIMENT. """
     api = comet_ml.api.API()
     experiment_ = api.get_experiment_by_id(experiment)
-    patch = experiment_.get_git_patch()
+    patch = experiment_.get_git_patch().decode()
     subprocess.run(f"echo {patch} | git apply")
+
+
+@app.command()
+def samples(
+    experiment: str,
+    dest: pathlib.Path = run._config.SAMPLES_PATH / lib.environment.bash_time_label(),
+):
+    """ Download all samples for an EXPERIMENT. """
+    api = comet_ml.api.API()
+    experiment_ = api.get_experiment_by_id(experiment)
+    asset_list = experiment_.get_asset_list(asset_type="audio")
+    dest.mkdir()
+    typer.echo(f"Downloading samples for experiment {experiment} into {dest}...")
+    for asset in tqdm.tqdm(asset_list):
+        file_path = dest / str(asset["fileName"]).replace(experiment, "")
+        file_path.write_bytes(experiment_.get_asset(asset["assetId"], return_type="binary"))
 
 
 if __name__ == "__main__":
