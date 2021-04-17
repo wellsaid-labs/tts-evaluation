+ Increase `dev_steps_per_epoch` to reduce metric variance by 8x + Fix zero output for `get_num_skipped` to "zero" instead of `empty`, either way it's the same. + Add new metric for `get_num_jumps`, to measure num tokens jumped over + Refactor `get_alignment_norm` and `get_alignment_std` for usage in visualization + Add "_Pick" for isolating interesting examples, and visualizing them. + Add speaker name to audio file name + Fix signal model bugs with incorrect parameterization

run/train/signal_model/_data.py
@@ -188,26 +188,30 @@ class DataProcessor(torch.utils.data.IterableDataset):
         """Sample slices from a batch of predicted spectrograms."""
         batch = self._make_spectrogram_model_batch(spans)
         with torch.no_grad():
-            spectrograms, _, _ = self.spectrogram_model(
+            params = lib.spectrogram_model.Params(
                 tokens=batch.encoded_phonemes.tensor,
                 speaker=batch.encoded_speaker.tensor,
-                target_frames=batch.spectrogram.tensor,
+                session=batch.encoded_session.tensor,
                 num_tokens=batch.encoded_phonemes.lengths,
                 tokens_mask=batch.encoded_phonemes_mask.tensor,
+            )
+            preds = self.spectrogram_model(
+                params=params,
+                target_frames=batch.spectrogram.tensor,
                 target_mask=batch.spectrogram_mask.tensor,
                 mode=lib.spectrogram_model.Mode.FORWARD,
             )
         num_frames = batch.spectrogram.lengths.sum().item()
         weights = batch.spectrogram.lengths.view(-1).float()
         num_batches = int(math.floor(num_frames / self.slice_size / self.batch_size))
-        get_spectrogram = lambda i: spectrograms[: batch.spectrogram.lengths[:, i], i]
+        get_spectrogram = lambda i: preds.frames[: batch.spectrogram.lengths[:, i], i]
         for _ in range(num_batches):
             indicies = torch.multinomial(weights, self.batch_size, replacement=True).tolist()
             slices = [self._slice(get_spectrogram(i), batch.audio[i]) for i in indicies]
             yield Batch(
                 batch=SpectrogramModelBatch(
                     **lib.utils.dataclass_as_dict(batch),
-                    predicted_spectrogram=SequenceBatch(spectrograms, batch.spectrogram.lengths),
+                    predicted_spectrogram=SequenceBatch(preds.frames, batch.spectrogram.lengths),
                 ),
                 indicies=indicies,
                 spectrogram=self._stack([s.spectrogram for s in slices]),

run/train/signal_model/_worker.py
@@ -508,12 +508,14 @@ def _visualize_inferred_end_to_end(
     num_tokens = batch.batch.encoded_phonemes.lengths[:, item]
     spectrogram_model = state.spectrogram_model_checkpoint.model
     spectrogram_model = spectrogram_model.train(False)
-    predicted_spectrogram, predicted_stop_token, predicted_alignments, _, _ = spectrogram_model(
+    params = lib.spectrogram_model.Params(
         tokens=batch.batch.encoded_phonemes.tensor[:num_tokens, item],
         speaker=batch.batch.encoded_speaker.tensor[:, item],
-        mode=lib.spectrogram_model.Mode.INFER,
+        session=batch.batch.encoded_session.tensor[:, item],
     )
-    splits = predicted_spectrogram.to(state.device).split(split_size)
+    preds = spectrogram_model(params=params, mode=lib.spectrogram_model.Mode.INFER)
+    preds = typing.cast(lib.spectrogram_model.Infer, preds)
+    splits = preds.frames.to(state.device).split(split_size)
     predicted = list(generate_waveform(state.model.module, splits))
     predicted = typing.cast(torch.Tensor, torch.cat(predicted, dim=-1))
     target = batch.batch.audio[item]
@@ -523,13 +525,13 @@ def _visualize_inferred_end_to_end(
     gold_spectrogram = batch.batch.spectrogram.tensor[:num_frames, item]
     figures = {
         dataset_label_("gold_spectrogram"): plot_mel_spectrogram(gold_spectrogram),
-        model_label_("predicted_spectrogram"): plot_mel_spectrogram(predicted_spectrogram),
-        model_label_("alignment"): lib.visualize.plot_alignments(predicted_alignments),
-        model_label_("stop_token"): lib.visualize.plot_logits(predicted_stop_token),
+        model_label_("predicted_spectrogram"): plot_mel_spectrogram(preds.frames),
+        model_label_("alignment"): lib.visualize.plot_alignments(preds.alignments),
+        model_label_("stop_token"): lib.visualize.plot_logits(preds.stop_tokens),
     }
     state.comet.log_figures(figures)
     audio = {
-        "predicted_griffin_lim_audio": lib.audio.griffin_lim(predicted_spectrogram.numpy()),
+        "predicted_griffin_lim_audio": lib.audio.griffin_lim(preds.frames.numpy()),
         "gold_griffin_lim_audio": lib.audio.griffin_lim(gold_spectrogram.numpy()),
         "predicted_signal_model_audio": predicted.cpu().numpy(),
         "gold_audio": target.numpy(),

run/train/spectrogram_model/_metrics.py
@@ -41,7 +41,7 @@ def get_num_skipped(
         torch.FloatTensor [batch_size]
     """
     if alignments.numel() == 0:
-        return torch.empty(alignments.shape[1], dtype=torch.float, device=alignments.device)
+        return torch.zeros(alignments.shape[1], device=alignments.device)
 
     indices = alignments.max(dim=2, keepdim=True).indices
     device = alignments.device
@@ -53,6 +53,33 @@ def get_num_skipped(
     num_skipped = num_skipped.masked_fill(~spectrogram_mask.unsqueeze(-1), 0).sum(dim=0)
     token_mask = token_mask.transpose(0, 1)
     return (num_skipped.masked_fill(~token_mask, -1) == 0).float().sum(dim=1)
+
+
+def get_num_jumps(
+    alignments: torch.Tensor, tokens_mask: torch.Tensor, spectrogram_mask: torch.Tensor
+) -> torch.Tensor:
+    """Given `alignments` from frames to tokens, the computes the number of "jumps" between frame
+    to frame, that would skip at least one token.
+
+    Args:
+        alignments (torch.FloatTensor [num_frames, batch_size, num_tokens])
+        token_mask (torch.BoolTensor [num_tokens, batch_size])
+        spectrogram_mask (torch.BoolTensor [num_frames, batch_size])
+
+    Returns:
+        torch.FloatTensor [batch_size]
+    """
+    if alignments.numel() == 0:
+        return torch.zeros(alignments.shape[1], device=alignments.device)
+
+    alignments = alignments.masked_fill(~tokens_mask.transpose(0, 1).unsqueeze(0), 0)
+    # [num_frames, batch_size, num_tokens] → [token_mask, batch_size]
+    indices = alignments.max(dim=2).indices
+    start = torch.cat([torch.zeros(1, alignments.shape[1]), indices[:-1]])
+    skip_size = indices - start
+    skip_size = skip_size.masked_fill(~spectrogram_mask, 0)
+    num_jumps = (skip_size.abs() > 1).float()
+    return num_jumps.sum(dim=0)
 
 
 """
@@ -121,6 +148,44 @@ def get_average_db_rms_level(
     return power_to_db(cumulative_power_rms_level / num_elements)
 
 
+def get_alignment_norm(
+    alignments: torch.Tensor, tokens_mask: torch.Tensor, spectrogram_mask: torch.Tensor
+) -> torch.Tensor:
+    """The inf-norm of an alignment. The more focused an alignment is the higher this metric. The
+    metric is bounded at [0, 1].
+
+    Args:
+        alignments (torch.FloatTensor [num_frames, batch_size, num_tokens])
+        token_mask (torch.BoolTensor [num_tokens, batch_size])
+        spectrogram_mask (torch.BoolTensor [num_frames, batch_size])
+
+    Returns:
+        torch.FloatTensor [batch_size]
+    """
+    alignments = alignments.masked_fill(~tokens_mask.transpose(0, 1).unsqueeze(0), 0)
+    alignments = alignments.norm(dim=2, p=math.inf)
+    return alignments.masked_fill(~spectrogram_mask, 0).sum(dim=0)
+
+
+def get_alignment_std(
+    alignments: torch.Tensor, tokens_mask: torch.Tensor, spectrogram_mask: torch.Tensor
+) -> torch.Tensor:
+    """This metric measures the standard deviation of an alignment. As the alignment is more
+    focused, this metrics goes to zero.
+
+    Args:
+        alignments (torch.FloatTensor [num_frames, batch_size, num_tokens])
+        token_mask (torch.BoolTensor [num_tokens, batch_size])
+        spectrogram_mask (torch.BoolTensor [num_frames, batch_size])
+
+    Returns:
+        torch.FloatTensor [batch_size]
+    """
+    alignments = alignments.masked_fill(~tokens_mask.transpose(0, 1).unsqueeze(0), 0)
+    alignments = lib.utils.get_weighted_std(alignments, dim=2)
+    return alignments.masked_fill(~spectrogram_mask, 0).sum(dim=0)
+
+
 _GetMetrics = typing.Dict[GetLabel, float]
 
 
@@ -139,6 +204,8 @@ class Metrics(_utils.Metrics):
             metric. The metric is bounded at [0, 1].
         ALIGNMENT_SKIPS: This metric assumes that each alignment focuses on one token. This measures
             the percentage of tokens skipped by the alignments.
+        ALIGNMENT_JUMPS: This metric assumes that each alignment focuses on one token. This measures
+            the percentage of token transitions that jump over a token.
         ALIGNMENT_STD: This metric measures the standard deviation of an alignment. As the alignment
             is more focused, this metrics goes to zero.
         AVERAGE_PREDICTED_RMS_LEVEL: The average loudness per predicted frame.
@@ -156,6 +223,7 @@ class Metrics(_utils.Metrics):
     (
         ALIGNMENT_NORM_SUM,
         ALIGNMENT_NUM_SKIPS,
+        ALIGNMENT_NUM_JUMPS,
         ALIGNMENT_STD_SUM,
         DATA_QUEUE_SIZE,
         MAX_FRAMES_PER_TOKEN,
@@ -188,6 +256,7 @@ class Metrics(_utils.Metrics):
 
     ALIGNMENT_NORM = partial(get_model_label, "alignment_norm")
     ALIGNMENT_SKIPS = partial(get_model_label, "alignment_skips")
+    ALIGNMENT_JUMPS = partial(get_model_label, "alignment_jumps")
     ALIGNMENT_STD = partial(get_model_label, "alignment_std")
     AVERAGE_PREDICTED_RMS_LEVEL = partial(get_model_label, "average_predicted_rms_level")
     AVERAGE_RELATIVE_SPEED = partial(get_model_label, "average_relative_speed")
@@ -198,7 +267,6 @@ class Metrics(_utils.Metrics):
     STOP_TOKEN_LOSS = partial(get_model_label, "stop_token_loss")
 
     TEXT_LENGTH_BUCKET_SIZE = 25
-    ALIGNMENT_NORM_TYPE = math.inf
 
     def __init__(
         self,
@@ -267,14 +335,12 @@ class Metrics(_utils.Metrics):
         """
         values: typing.Dict[str, float] = collections.defaultdict(float)
         tokens_mask = batch.encoded_phonemes_mask.tensor
-        alignments = alignments.masked_fill(~tokens_mask.transpose(0, 1).unsqueeze(0), 0)
-        _to_list = lambda a: self._to_list(a.masked_fill(~spectrogram_mask, 0).sum(dim=0))
-
-        for span, num_skipped, alignment_std, alignment_norm, length, has_reached_max in zip(
+        for span, num_skipped, num_jumps, std, norm, length, has_reached_max in zip(
             batch.spans,
             self._to_list(get_num_skipped(alignments, tokens_mask, spectrogram_mask)),
-            _to_list(lib.utils.get_weighted_std(alignments, dim=2)),
-            _to_list(alignments.norm(p=self.ALIGNMENT_NORM_TYPE, dim=2)),
+            self._to_list(get_num_jumps(alignments, tokens_mask, spectrogram_mask)),
+            self._to_list(get_alignment_std(alignments, tokens_mask, spectrogram_mask)),
+            self._to_list(get_alignment_norm(alignments, tokens_mask, spectrogram_mask)),
             self._to_list(spectrogram_lengths),
             itertools.repeat(False) if reached_max is None else self._to_list(reached_max),
         ):
@@ -285,9 +351,10 @@ class Metrics(_utils.Metrics):
             if not has_reached_max:
                 for suffix in ["", f"/{span.speaker.label}"]:
                     format_ = lambda s: f"{s}{suffix}"
-                    values[format_(self.ALIGNMENT_NORM_SUM)] += alignment_norm
+                    values[format_(self.ALIGNMENT_NORM_SUM)] += norm
                     values[format_(self.ALIGNMENT_NUM_SKIPS)] += num_skipped
-                    values[format_(self.ALIGNMENT_STD_SUM)] += alignment_std
+                    values[format_(self.ALIGNMENT_NUM_JUMPS)] += num_jumps
+                    values[format_(self.ALIGNMENT_STD_SUM)] += std
                     values[format_(self.NUM_FRAMES_PREDICTED)] += length
 
         return dict(values)
@@ -364,6 +431,7 @@ class Metrics(_utils.Metrics):
                 self.ALIGNMENT_NORM: div(self.ALIGNMENT_NORM_SUM, self.NUM_FRAMES_PREDICTED),
                 self.ALIGNMENT_STD: div(self.ALIGNMENT_STD_SUM, self.NUM_FRAMES_PREDICTED),
                 self.ALIGNMENT_SKIPS: div(self.ALIGNMENT_NUM_SKIPS, self.NUM_TOKENS),
+                self.ALIGNMENT_JUMPS: div(self.ALIGNMENT_NUM_JUMPS, self.NUM_TOKENS),
                 self.AVERAGE_RELATIVE_SPEED: div(self.NUM_FRAMES_PREDICTED, self.NUM_FRAMES),
                 self.STOP_TOKEN_ACCURACY: div(self.NUM_CORRECT_STOP_TOKEN, self.NUM_FRAMES),
                 self.STOP_TOKEN_LOSS: div(self.STOP_TOKEN_LOSS_SUM, self.NUM_FRAMES),

run/train/spectrogram_model/_worker.py
@@ -5,6 +5,7 @@ import logging
 import math
 import pathlib
 import random
+import types
 import typing
 from functools import partial
 from itertools import chain
@@ -43,7 +44,11 @@ from run.train._utils import (
     set_run_seed,
 )
 from run.train.spectrogram_model._data import Batch, DataProcessor, InputEncoder
-from run.train.spectrogram_model._metrics import Metrics, get_average_db_rms_level
+from run.train.spectrogram_model._metrics import (
+    Metrics,
+    get_alignment_norm,
+    get_average_db_rms_level,
+)
 
 logger = logging.getLogger(__name__)
 torch.optim.Adam.__init__ = configurable_(torch.optim.Adam.__init__)
@@ -100,7 +105,7 @@ class _State:
             [p.script for p in passages],
             DATASET_PHONETIC_CHARACTERS,
             list(train_dataset.keys()) + list(dev_dataset.keys()),
-            [(p.speaker, p.session)for p in passages],
+            [(p.speaker, p.session) for p in passages],
         )
 
         label = partial(get_dataset_label, cadence=Cadence.STATIC, type_=DatasetType.TRAIN)
@@ -425,7 +430,42 @@ def _run_step(
     args.metrics.update(values)
 
 
-def _visualize_inferred(args: _HandleBatchArgs, preds: lib.spectrogram_model.Infer):
+def _min_alignment_norm(
+    args: _HandleBatchArgs, preds: lib.spectrogram_model.Infer, spectrogram_mask: torch.Tensor
+) -> int:
+    """ Get the index of the prediction that has the smallest alignment norm."""
+    tokens_mask = args.batch.encoded_phonemes_mask.tensor
+    return int(torch.argmin(get_alignment_norm(preds.alignments, tokens_mask, spectrogram_mask)))
+
+
+def _max_num_frames_diff(args: _HandleBatchArgs, preds: lib.spectrogram_model.Infer, *_) -> int:
+    """ Get the index of the prediction that most deviates from the original spectrogram length."""
+    return int(torch.argmax((args.batch.spectrogram.lengths - preds.lengths).abs()))
+
+
+def _random_sequence(args: _HandleBatchArgs, *_) -> int:
+    """ Get a random batch index. """
+    return random.randint(0, len(args.batch) - 1)
+
+
+class _Pick(typing.Protocol):
+    """ Get a batch index given the arguments and predictions. """
+
+    def __call__(
+        self,
+        args: _HandleBatchArgs,
+        preds: lib.spectrogram_model.Infer,
+        spectrogram_mask: torch.Tensor,
+    ) -> int:
+        ...
+
+
+def _visualize_inferred(
+    args: _HandleBatchArgs,
+    preds: lib.spectrogram_model.Infer,
+    spectrogram_mask: torch.Tensor,
+    pick: _Pick = _random_sequence,
+):
     """Run in inference mode and visualize results.
 
     TODO: Visualize any related text annotations.
@@ -433,7 +473,8 @@ def _visualize_inferred(args: _HandleBatchArgs, preds: lib.spectrogram_model.Inf
     if not is_master():
         return
 
-    item = random.randint(0, len(args.batch) - 1)
+    pick_label = typing.cast(types.MethodType, pick).__name__
+    item = pick(args, preds, spectrogram_mask)
     num_frames = int(args.batch.spectrogram.lengths[0, item].item())
     num_frames_predicted = int(preds.lengths[0, item].item())
     text_length = int(args.batch.encoded_phonemes.lengths[0, item].item())
@@ -444,8 +485,10 @@ def _visualize_inferred(args: _HandleBatchArgs, preds: lib.spectrogram_model.Inf
     predicted_alignments = preds.alignments[:num_frames_predicted, item, :text_length]
     predicted_stop_token = preds.stop_tokens[:num_frames_predicted, item]
 
-    model = partial(get_model_label, cadence=args.cadence)
-    dataset = partial(get_dataset_label, cadence=args.cadence, type_=args.dataset_type)
+    model = lambda n: get_model_label(f"{n}/{pick_label}", cadence=args.cadence)
+    dataset = lambda n: get_dataset_label(
+        f"{n}/{pick_label}", cadence=args.cadence, type_=args.dataset_type
+    )
     figures = {
         dataset("gold_spectrogram"): lib.visualize.plot_mel_spectrogram(gold_spectrogram),
         model("predicted_spectrogram"): lib.visualize.plot_mel_spectrogram(predicted_spectrogram),
@@ -466,11 +509,16 @@ def _visualize_inferred(args: _HandleBatchArgs, preds: lib.spectrogram_model.Inf
         session=args.batch.spans[item].session,
         predicted_loudness=get_average_db_rms_level(predicted_spectrogram.unsqueeze(1)).item(),
         gold_loudness=get_average_db_rms_level(gold_spectrogram.unsqueeze(1)).item(),
+        pick_function=pick_label,
     )
 
 
 def _run_inference(args: _HandleBatchArgs):
-    """Run the model in inference mode, and measure it's results."""
+    """Run the model in inference mode, and measure it's results.
+
+    TODO: Over multiple steps, track the example with the smallest `_min_alignment_norm` or largest
+    `_max_num_frames_diff`, and visualize it.
+    """
     args.timer.record_event(args.timer.MODEL_FORWARD)
     params = lib.spectrogram_model.Params(
         tokens=args.batch.encoded_phonemes.tensor,
@@ -481,13 +529,14 @@ def _run_inference(args: _HandleBatchArgs):
     )
     preds = args.state.model.module(params, mode=lib.spectrogram_model.Mode.INFER)
     preds = typing.cast(lib.spectrogram_model.Infer, preds)
+    mask = lengths_to_mask(preds.lengths, device=preds.lengths.device).transpose(0, 1)
 
     if args.visualize:
         args.timer.record_event(args.timer.VISUALIZE_PREDICTIONS)
-        _visualize_inferred(args, preds)
+        for pick in [_random_sequence, _max_num_frames_diff, _min_alignment_norm]:
+            _visualize_inferred(args, preds, mask, pick)
 
     args.timer.record_event(args.timer.MEASURE_METRICS)
-    mask = lengths_to_mask(preds.lengths, device=preds.lengths.device).transpose(0, 1)
     values: _utils.MetricsValues = {
         **args.metrics.get_dataset_values(args.batch, preds.reached_max),
         **args.metrics.get_alignment_values(

tests/run/train/test__utils.py
@@ -12,7 +12,7 @@ from matplotlib import pyplot
 import lib
 import run
 from run._config import Cadence, DatasetType, get_dataset_label
-from run.data._loader import Alignment
+from run.data._loader import Alignment, Speaker
 from tests._utils import TEST_DATA_PATH, make_passage
 
 TEST_DATA_PATH = TEST_DATA_PATH / "audio"
@@ -123,6 +123,7 @@ def test_comet_ml_experiment():
         comet.log_html_audio(
             metadata="random metadata",
             audio={"predicted_audio": torch.rand(100), "gold_audio": torch.rand(100)},
+            speaker=Speaker(""),
         )
         figure = pyplot.figure()
         pyplot.close(figure)