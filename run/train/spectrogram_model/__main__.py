565d00439cc80e446aa3e3e86098f9d25c947a43
Michael Petrochuk <petrochukm@gmail.com>
2021-04-17 17:10:59 -0700

    Fix typing error

--- run/train/spectrogram_model/__main__.py
+++ run/train/spectrogram_model/__main__.py
@@ -47,7 +47,7 @@ def _make_configuration(
     dev_steps_per_epoch = 1 if debug else 128
     dev_oversample_ratio = 4
     # NOTE: Oversample the development dataset in order to reduce the variance of the results.
-    dev_steps_to_train_steps = batch_size_ratio * round(ratio) / dev_oversample_ratio
+    dev_steps_to_train_steps = int(round(batch_size_ratio * ratio * (1 / dev_oversample_ratio)))
     train_steps_per_epoch = 1 if debug else dev_steps_per_epoch * dev_steps_to_train_steps
     assert train_batch_size % lib.distributed.get_device_count() == 0
     assert dev_batch_size % lib.distributed.get_device_count() == 0
