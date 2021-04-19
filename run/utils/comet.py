601fdd4f0094f67405f522c27262186cf2f10d7c
Michael Petrochuk <petrochukm@gmail.com>
2021-04-18 19:28:37 -0700

    Fix `run.utils.comet patch`

--- run/utils/comet.py
+++ run/utils/comet.py
@@ -2,6 +2,8 @@
 import math
 import pathlib
 import subprocess
+import typing
+import zipfile
 
 import comet_ml
 import tqdm
@@ -13,8 +15,8 @@ import run
 app = typer.Typer(context_settings=dict(max_content_width=math.inf))
 
 
-@app.command()
-def checkout(experiment: str):
+@app.command(context_settings={"allow_extra_args": True, "ignore_unknown_options": True})
+def checkout(context: typer.Context, experiment: str, overwrite: bool = False):
     """ Updates the files in the working directory to reproduce a EXPERIMENT. """
     api = comet_ml.api.API()
     experiment_ = api.get_experiment_by_id(experiment)
@@ -22,16 +24,44 @@ def checkout(experiment: str):
     typer.secho(f"Checking out experiment {experiment_.get_name()}...", fg=typer.colors.MAGENTA)
     subprocess.run(f"git checkout {git['branch']}", shell=True, check=True)
     subprocess.run(f"git checkout {git['parent']}", shell=True, check=True)
-    patch.__wrapped__(experiment)  # type: ignore
+    patch.__wrapped__(context, experiment, overwrite=overwrite)  # type: ignore
 
 
-@app.command()
-def patch(experiment: str):
-    """ Apply git patch created for a EXPERIMENT. """
+@app.command(context_settings={"allow_extra_args": True, "ignore_unknown_options": True})
+def patch(
+    context: typer.Context,
+    experiment: str,
+    zip_file_name: str = "patch.zip",
+    overwrite: bool = False,
+):
+    """Apply git patch created for a EXPERIMENT.
+
+    Examples:
+
+    $ python -m run.utils.comet patch XXXXXXXX --overwrite --include="*.py"
+
+    $ python -m run.utils.comet patch XXXXXXXX --overwrite --3way
+
+    $ python -m run.utils.comet patch XXXXXXXX --overwrite --ignore-space-change \
+--ignore-whitespace --reject
+    """
     api = comet_ml.api.API()
+    patch_file_name = "git_diff.patch"
     experiment_ = api.get_experiment_by_id(experiment)
-    patch = experiment_.get_git_patch().decode()
-    subprocess.run(f"echo {patch} | git apply")
+    patch = typing.cast(bytes, experiment_.get_git_patch())
+    zip_file_path = pathlib.Path(zip_file_name)
+    if zip_file_path.exists() and not overwrite:
+        typer.echo(f"File {zip_file_path} already exists.")
+        raise typer.Exit()
+    zip_file_path.write_bytes(patch)
+    with zipfile.ZipFile(zip_file_name) as zip_:
+        with zip_.open(patch_file_name) as patch:
+            patch_file_path = pathlib.Path(patch_file_name)
+            if patch_file_path.exists() and not overwrite:
+                typer.echo(f"File {patch_file_path} already exists.")
+                raise typer.Exit()
+            patch_file_path.write_bytes(patch.read())
+    subprocess.run(["git", "apply", patch_file_name] + context.args)
 
 
 @app.command()
