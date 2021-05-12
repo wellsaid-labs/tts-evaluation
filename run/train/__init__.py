bdf89a896e0da795e2d2ade2bcd40dbee2d9ee1f
Michael Petrochuk <petrochukm@gmail.com>
2021-05-11 17:24:18 -0700

    Fix imports for `signal_model`

--- run/train/__init__.py
+++ run/train/__init__.py
@@ -1,5 +1,5 @@
 # NOTE: Other `run` modules import `_utils`; therefore, they need to be imported first.
 from run.train import _utils  # isort: skip
-from run.train import spectrogram_model
+from run.train import signal_model, spectrogram_model
 
-__all__ = ["spectrogram_model", "_utils"]
+__all__ = ["signal_model", "spectrogram_model", "_utils"]
---
+++ run/train/signal_model/__init__.py
@@ -0,0 +1,3 @@
+from run.train.spectrogram_model import __main__, _data, _metrics, _worker
+
+__all__ = ["__main__", "_data", "_metrics", "_worker"]
