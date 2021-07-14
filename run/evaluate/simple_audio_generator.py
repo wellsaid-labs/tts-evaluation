15be5be7c54bb0cc31fc3cbdf8be0712dfdf8bd8
rhyanjohnson <rhyanjohnson5@gmail.com>
2021-07-14 11:24:30 -0500

    Clean up streamlit app. Remove unused objects. Merge similar code chunks into helper function. Change layout. Change num clips selector from slider to number_input.

--- run/evaluate/simple_audio_generator.py
+++ run/evaluate/simple_audio_generator.py
@@ -1,11 +1,8 @@
-""" A workbook to stream clip generation.
+""" A workbook to generate audio for quick evaluations.
 
 Usage:
     $ PYTHONPATH=. streamlit run run/evaluate/simple_audio_generator.py --runner.magicEnabled=false
 """
-import logging
-import multiprocessing
-import threading
 import typing
 
 import streamlit as st
@@ -28,10 +25,26 @@ DEFAULT_SCRIPT = (
     " your gut. Don’t overthink it. And allow yourself a little room to play."
 )
 
-st.set_page_config(layout="wide")
-lib.environment.set_basic_logging_config(reset=True)
-logger = logging.getLogger(__name__)
-Event = typing.Union[multiprocessing.synchronize.Event, threading.Event]
+
+def _get_speaker_name(speaker):
+    return typing.cast(Speaker, speaker).name.split()[0]
+
+
+def _generate_audio(tts, script, speaker, session, num_clips, session_count):
+    paths = []
+    speaker_name = _get_speaker_name(speaker)
+
+    for clip_count, audio in enumerate(text_to_speech(tts, script, speaker, session, num_clips)):
+        audio_web_path = audio_to_web_path(
+            audio, name="%s_session%d_%d.wav" % (speaker_name, session_count, clip_count + 1)
+        )
+        st_html(f'<audio controls src="{web_path_to_url(audio_web_path)}"></audio>')
+        url = web_path_to_url(audio_web_path)
+        st_html(f'<a href="{url}" download="{audio_web_path.name}">Download Generated Audio</a>')
+        st.text("")
+        paths.append(audio_web_path)
+
+    return paths
 
 
 def main():
@@ -50,17 +63,18 @@ def main():
     speakers = sorted(tts.input_encoder.speaker_encoder.index_to_token)
     speaker = st.selectbox("Speaker", options=speakers, format_func=format_speaker)
 
-    all_sessions = st.checkbox("Sample all sessions")
     sessions = tts.input_encoder.session_encoder.index_to_token
     sessions = sorted([sesh for spk, sesh in sessions if spk == speaker], key=natural_keys)
+
+    all_sessions = st.checkbox("Sample all %d sessions" % len(sessions))
     session = st.selectbox("Session", options=["All"] if all_sessions else sessions)
     script = st.text_area("Script", value=DEFAULT_SCRIPT, height=300)
     st.info(f"The script has {len(script):,} character(s).")
 
-    num_clips = st.slider(
+    num_clips = st.number_input(
         "Number of Clips",
-        min_value=0,
-        max_value=1 if all_sessions else 10,
+        min_value=1,
+        max_value=1 if all_sessions else None,
         value=1 if all_sessions else 5,
     )
 
@@ -68,35 +82,20 @@ def main():
         st.stop()
 
     paths = []
-    speaker_name = typing.cast(Speaker, speaker).name.split()[0]
     with st.spinner("Generating Audio..."):
         if all_sessions:
             for s, sesh in enumerate(sessions):
-                for i, audio in enumerate(text_to_speech(tts, script, speaker, sesh, num_clips)):
-                    audio_web_path = audio_to_web_path(
-                        audio, name="%s_session%d.wav" % (speaker_name, s + 1)
-                    )
-                    st_html(f'<audio controls src="{web_path_to_url(audio_web_path)}"></audio>')
-                    url = web_path_to_url(audio_web_path)
-                    st_html(
-                        f'<a href="{url}" download="{audio_web_path.name}">Download Generated Audio</a>'
-                    )
-                    st.text("")
-                    paths.append(audio_web_path)
+                paths.extend(_generate_audio(tts, script, speaker, sesh, num_clips, s + 1))
         else:
-            for i, audio in enumerate(text_to_speech(tts, script, speaker, session, num_clips)):
-                audio_web_path = audio_to_web_path(audio, name="%s_%d.wav" % (speaker_name, i))
-                st_html(f'<audio controls src="{web_path_to_url(audio_web_path)}"></audio>')
-                url = web_path_to_url(audio_web_path)
-                st_html(
-                    f'<a href="{url}" download="{audio_web_path.name}">Download Generated Audio</a>'
+            paths.extend(
+                _generate_audio(
+                    tts, script, speaker, session, num_clips, sessions.index(session) + 1
                 )
-                st.text("")
-                paths.append(audio_web_path)
+            )
 
     with st.spinner("Making Zipfile..."):
         st.text("")
-        zip_name = "%s_samples.zip" % speaker_name
+        zip_name = "%s_samples.zip" % _get_speaker_name(speaker)
         st_html(paths_to_html_download_link(zip_name, "Download All (zip)", paths))
 
     st.success(f"Finished! {lib.utils.mazel_tov()}")
