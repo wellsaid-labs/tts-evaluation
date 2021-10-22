3161735ddd60d22eb196e17e4d25c7f0e2cb18d0
rhyanjohnson <rhyanjohnson5@gmail.com>
2021-10-22 16:35:30 -0500

    Move speaker and session selection outside form so they update. Allow multiple session selection.

--- run/evaluate/simple_audio_generator.py
+++ run/evaluate/simple_audio_generator.py
@@ -38,10 +38,9 @@ def main():
     with st.spinner("Loading checkpoint(s)..."):
         tts = load_tts(checkpoints_key)
 
-    frm = st.form(key="form")
     format_speaker: typing.Callable[[Speaker], str] = lambda s: s.label
     speakers = sorted(tts.input_encoder.speaker_encoder.index_to_token)
-    speaker: Speaker = frm.selectbox("Speaker", options=speakers, format_func=format_speaker)
+    speaker: Speaker = st.selectbox("Speaker", options=speakers, format_func=format_speaker)
     assert speaker.name is not None
     speaker_name = speaker.name.split()[0].lower()
 
@@ -49,9 +48,13 @@ def main():
     spk_sesh = tts.input_encoder.session_encoder.index_to_token
     sessions = sorted([sesh for spk, sesh in spk_sesh if spk == speaker], key=natural_keys)
 
-    all_sessions: bool = frm.checkbox("Sample all %d sessions" % len(sessions))
-    session: Session = frm.selectbox("Session", options=["All"] if all_sessions else sessions)
-    script: str = frm.text_area("Script", value=DEFAULT_SCRIPT, height=300)
+    all_sessions: bool = st.checkbox("Sample all %d sessions" % len(sessions))
+    selected_sessions: typing.List[Session] = st.multiselect(
+        "Session(s)", options=["All"] if all_sessions else sessions
+    )
+
+    frm = st.form(key="form")
+    script: str = frm.text_area("Script", value=DEFAULT_SCRIPT, height=150)
 
     label = "Number of Clips Per Session"
     num_clips: int = frm.number_input(label, min_value=1, max_value=None, value=5)
@@ -59,11 +62,9 @@ def main():
     if not frm.form_submit_button("Submit"):
         return
 
-    st.info(f"The script has {len(script):,} character(s).")
-
     paths = []
     with st.spinner("Generating Audio..."):
-        inputs = [(script, speaker, s) for s in (sessions if all_sessions else [session])]
+        inputs = [(script, speaker, s) for s in selected_sessions]
         inputs = [i for i in inputs for _ in range(num_clips)]
         for i, generated in enumerate(batch_text_to_speech(tts, inputs)):
             clip_num = i % num_clips + 1
@@ -76,7 +77,7 @@ def main():
     with st.spinner("Making Zipfile..."):
         st.text("")
         zip_name = f"{speaker_name}_samples.zip"
-        st_html(paths_to_html_download_link(zip_name, "Download All (zip)", paths))
+        st_html(paths_to_html_download_link(zip_name, f"📁 Download All {len(paths)} (zip)", paths))
 
     st.success(f"Finished! {lib.utils.mazel_tov()}")
 
