763d233e8182b1c86062ae98b5a103fa776c9eaa
Michael Petrochuk <petrochukm@gmail.com>
2022-01-18 15:45:37 -0800

    Update german voice

--- run/data/_loader/german/wsl.py
+++ run/data/_loader/german/wsl.py
@@ -3,7 +3,9 @@ from functools import partial
 from run.data._loader.data_structures import make_de_speaker
 from run.data._loader.utils import Speaker, wsl_gcs_dataset_loader
 
-MITEL_GERMAN__CUSTOM_VOICE = make_de_speaker("Mitel - German", "Mitel (German Custom Voice)")
+MITEL_GERMAN__CUSTOM_VOICE = make_de_speaker(
+    "mitel__custom_voice_de_de", "Mitel (German Custom Voice)"
+)
 
 
 _wsl_speakers = [s for s in locals().values() if isinstance(s, Speaker)]
