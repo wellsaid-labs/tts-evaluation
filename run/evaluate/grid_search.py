92f4f73195bce737d7fada33700da47df44830bd
Michael Petrochuk <petrochukm@gmail.com>
2022-02-07 16:21:31 -0800

    + Allow for more flexible selection of checkpoints + Factor out DEFAULT_SCRIPT + Make code a bit more succinct + Add back st_datatable + Use f-string + Add back index

--- run/evaluate/grid_search.py
+++ run/evaluate/grid_search.py
@@ -18,16 +18,17 @@ from typing import cast
 
 import pandas as pd
 import streamlit as st
+from streamlit_datatable import st_datatable
 
 import lib
 import run
-from lib.environment import PT_EXTENSION, load
+from lib.environment import PT_EXTENSION, ROOT_PATH, load
 from run import train
 from run._config import SIGNAL_MODEL_EXPERIMENTS_PATH, SPECTROGRAM_MODEL_EXPERIMENTS_PATH
 from run._streamlit import (
+    DEFAULT_SCRIPT,
     audio_to_web_path,
     paths_to_html_download_link,
-    st_data_frame,
     st_html,
     web_path_to_url,
 )
@@ -36,63 +37,22 @@ from run._tts import TTSPackage, text_to_speech
 st.set_page_config(layout="wide")
 
 
-DEFAULT_SCRIPT = (
-    "Your creative life will evolve in ways that you can’t possibly imagine. Trust"
-    " your gut. Don’t overthink it. And allow yourself a little room to play."
-)
-
-ST_FORMAT_FILE: typing.Callable[[pathlib.Path], str] = (
-    lambda p: f"{p.name}/" if p.is_dir() else p.name
-)
-
-
 def path_label(path: pathlib.Path) -> str:
     """Get a short label for `path`."""
-    return f"{path.parent.name}/{path.name}"
+    suffix = "/" if path.is_dir() else ""
+    return str(path.relative_to(ROOT_PATH)) + suffix
 
 
-def st_select_paths(label: str, dir: pathlib.Path) -> pathlib.Path:
-    """Display a subdirectory selector for the directory `dir`. Then display a checkpoint selector
-    for the selected subdirectory `subdir`. Return list of checkpoint paths."""
-    options = [p for p in dir.iterdir()]
-    options.sort(reverse=True)
-    path = typing.cast(
-        pathlib.Path,
-        st.selectbox("%s Training Path" % label, options=options, format_func=ST_FORMAT_FILE),
-    )
-    subdirs = [p for p in path.iterdir()]
-    subdirs.sort(reverse=True)
-    assert subdirs, "Subdirs empty:\t%s" % path
-    subdir = typing.cast(
-        pathlib.Path,
-        st.selectbox("%s Checkpoints Path" % label, options=subdirs, format_func=ST_FORMAT_FILE)
-        if subdirs
-        else ["None Available"],
-    )
-
-    return subdir
-
-
-def st_select_checkpoints(
-    label: str, subdir: pathlib.Path, num_ckpts=3
-) -> typing.List[pathlib.Path]:
-    ckpt_options = list(subdir.glob(f"**/*{PT_EXTENSION}"))
-    ckpt_options.sort(reverse=True)
-    ckpts_selected = typing.cast(
-        typing.List[pathlib.Path],
-        st.multiselect(
-            label="%s Checkpoint(s)" % label,
-            options=ckpt_options,
-            default=ckpt_options[:num_ckpts],
-            format_func=ST_FORMAT_FILE,
-        ),
-    )
-    ckpts_selected.sort()
-    st.info(
-        "Selected %s Checkpoint(s):\n" % label
-        + "".join(["\n - " + path_label(p) for p in ckpts_selected])
-    )
-    return ckpts_selected
+def st_select_paths(label: str, dir: pathlib.Path, suffix: str) -> typing.List[pathlib.Path]:
+    """Display a path selector for the directory `dir`."""
+    options = [p for p in dir.glob("**/*") if p.suffix == suffix or p.is_dir()] + [dir]
+    options = sorted(options)
+    paths = st.multiselect(label, options=options, format_func=path_label)
+    paths = cast(typing.List[pathlib.Path], paths)
+    paths = [f for p in paths for f in ([p] if p.is_file() else list(p.glob(f"**/*{suffix}")))]
+    if len(paths) > 0:
+        st.info(f"Selected {label}:\n" + "".join(["\n - " + path_label(p) for p in paths]))
+    return paths
 
 
 def get_sample_sessions(
@@ -115,95 +75,71 @@ def main():
         "set of model(s), a speaker, a recording session, and a script."
     )
     run._config.configure()
-    get_paths = functools.partial(st_select_paths)
-    sig_ckpt_paths = get_paths("Signal", SIGNAL_MODEL_EXPERIMENTS_PATH)
-    sig_ckpts_selected = st_select_checkpoints("Signal", sig_ckpt_paths, 1)
-    spec_ckpt_paths = get_paths("Spectrogram", SPECTROGRAM_MODEL_EXPERIMENTS_PATH)
+    get_paths = functools.partial(st_select_paths, suffix=PT_EXTENSION)
+    sig_paths = get_paths("Signal Checkpoints(s)", SIGNAL_MODEL_EXPERIMENTS_PATH)
+    spec_paths = get_paths("Spectrogram Checkpoints(s)", SPECTROGRAM_MODEL_EXPERIMENTS_PATH)
 
     with st.form(key="data_form"):
-        spec_ckpts_selected = st_select_checkpoints("Spectrogram", spec_ckpt_paths, 1)
-
         speakers = list(run._config.DATASETS.keys())
         speakers.sort()
         is_all = st.sidebar.checkbox("Select all speakers by default")
         format_: typing.Callable[[run.data._loader.Speaker], str] = lambda s: s.label
         default = speakers if is_all else speakers[:1]
-        speakers = typing.cast(
-            typing.List[run.data._loader.Speaker],
-            st.multiselect("Speaker(s)", options=speakers, format_func=format_, default=default),
-        )
-        max_sessions = typing.cast(
-            int, st.number_input("Maximum Recording Sessions", min_value=1, value=1, step=1)
-        )
+        label = "Speaker(s)"
+        speakers = st.multiselect(label, options=speakers, format_func=format_, default=default)
+        speakers = cast(typing.List[run.data._loader.Speaker], speakers)
+        max_sessions = st.number_input("Maximum Recording Sessions", min_value=1, value=1, step=1)
+        max_sessions = cast(int, max_sessions)
         scripts = st.text_area("Script(s)", value=DEFAULT_SCRIPT)
         scripts = [s.strip() for s in scripts.split("\n") if len(s.strip()) > 0]
         file_name = st.text_input(label="Zipfile Name", value="audio(s)")
         submit = st.form_submit_button(label="Generate")
 
-    if submit:
-        with st.spinner("Loading checkpoints..."):
-            spec_ckpts = [
-                cast(train.spectrogram_model._worker.Checkpoint, load(p))
-                for p in spec_ckpts_selected
-            ]
-            spec_export = [
-                (c.export(), c.comet_experiment_key, c.step, p)
-                for c, p in zip(spec_ckpts, spec_ckpts_selected)
-            ]
-            sig_ckpts = [
-                cast(train.signal_model._worker.Checkpoint, load(p)) for p in sig_ckpts_selected
-            ]
-            sig_export = [
-                (c.export(), c.comet_experiment_key, c.step, p)
-                for c, p in zip(sig_ckpts, sig_ckpts_selected)
-            ]
+    if not submit:
+        return
 
-        rows = []
-        paths = []
-        bar = st.progress(0)
-        sessions: typing.List[typing.Tuple[run.data._loader.Speaker, run.data._loader.Session]]
-        sessions = list(set(s for c in spec_ckpts for s in c.input_encoder.session_encoder.vocab))
-        sessions_sample = get_sample_sessions(speakers, sessions, max_sessions)
-        iter_ = list(itertools.product(sessions_sample, spec_export, sig_export, scripts))
-        for i, (
-            (speaker, session),
-            spec_items,
-            sig_items,
-            script,
-        ) in enumerate(iter_):
-            (input_encoder, spec_model), spec_comet_key, spec_step, spec_path = spec_items
-            (sig_model, sig_comet_key, sig_step, sig_path) = sig_items
-            package = TTSPackage(
-                input_encoder,
-                spec_model,
-                sig_model,
-                spec_comet_key,
-                spec_step,
-                sig_comet_key,
-                sig_step,
-            )
-            audio = text_to_speech(package, script, speaker, session)
-            sesh = str(session).replace("/", "__")
-            name = f"spec={spec_path.stem},sig={sig_path.stem},spk={speaker.label},"
-            name += f"sesh={sesh},script={id(script)}.wav"
-            audio_web_path = audio_to_web_path(audio, name=name)
-            row = {
-                "Audio": f'<audio controls src="{web_path_to_url(audio_web_path)}"></audio>',
-                "Spectrogam Model": path_label(spec_path),
-                "Signal Model": path_label(sig_path),
-                "Speaker": speaker.label,
-                "Session": session,
-                "Script": f"'{script[:25]}...'",
-            }
-            rows.append(row)
-            paths.append(audio_web_path)
-            bar.progress(len(rows) / len(iter_))
-        bar.empty()
-        st_data_frame(pd.DataFrame(rows))
+    with st.spinner("Loading checkpoints..."):
+        spec_ckpts = [cast(train.spectrogram_model._worker.Checkpoint, load(p)) for p in spec_paths]
+        spec_export = [(c.export(), p) for c, p in zip(spec_ckpts, spec_paths)]
+        sig_ckpts = [cast(train.signal_model._worker.Checkpoint, load(p)) for p in sig_paths]
+        sig_export = [(c.export(), p) for c, p in zip(sig_ckpts, sig_paths)]
 
-        with st.spinner("Making Zipfile..."):
-            st.text("")
-            st_html(paths_to_html_download_link("%s.zip" % file_name, "Download Audio(s)", paths))
+    rows = []
+    paths = []
+    bar = st.progress(0)
+    sessions: typing.List[typing.Tuple[run.data._loader.Speaker, run.data._loader.Session]]
+    sessions = list(set(s for c in spec_ckpts for s in c.input_encoder.session_encoder.vocab))
+    sessions_sample = get_sample_sessions(speakers, sessions, max_sessions)
+    iter_ = list(itertools.product(sessions_sample, spec_export, sig_export, scripts))
+    for i, (
+        (speaker, session),
+        ((input_encoder, spec_model), spec_path),
+        (sig_model, sig_path),
+        script,
+    ) in enumerate(iter_):
+        package = TTSPackage(input_encoder, spec_model, sig_model)
+        audio = text_to_speech(package, script, speaker, session)
+        sesh = str(session).replace("/", "__")
+        name = f"i={i},spec={spec_path.stem},sig={sig_path.stem},spk={speaker.label},"
+        name += f"sesh={sesh},script={id(script)}.wav"
+        audio_web_path = audio_to_web_path(audio, name=name)
+        row = {
+            "Audio": f'<audio controls src="{web_path_to_url(audio_web_path)}"></audio>',
+            "Spectrogam Model": path_label(spec_path),
+            "Signal Model": path_label(sig_path),
+            "Speaker": speaker.label,
+            "Session": session,
+            "Script": f"'{script[:25]}...'",
+        }
+        rows.append(row)
+        paths.append(audio_web_path)
+        bar.progress(len(rows) / len(iter_))
+    bar.empty()
+    st_datatable(pd.DataFrame(rows))
+
+    with st.spinner("Making Zipfile..."):
+        st.text("")
+        st_html(paths_to_html_download_link(f"{file_name}.zip", "Download Audio(s)", paths))
 
 
 if __name__ == "__main__":
