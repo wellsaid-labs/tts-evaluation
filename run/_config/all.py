9f4bc97049a9f4780957a9282c4789bbdfe259ea
Michael Petrochuk <petrochukm@gmail.com>
2022-03-06 12:11:39 -0800

    Ensure configuration is setup correctly, profile code.

--- run/_config/all.py
+++ run/_config/all.py
@@ -1,8 +1,14 @@
+import sys
+
+import config
+
 from run._config import audio, data, environment, lang, models
 
 
 def configure():
     """Configure required modules."""
+    # TODO: Consider removing after configuration is setup.
+    sys.setprofile(config.profile)
     environment.configure()
     audio.configure()
     models.configure()
