6ef9d8b174e16bac1af8547969e72d034e5f42c1
Michael Petrochuk <petrochukm@gmail.com>
2022-03-08 16:47:05 -0800

    + Update make_passage to be more flexible + Test `_include_span`

--- tests/run/test__utils.py
+++ tests/run/test__utils.py
@@ -5,9 +5,9 @@ import pytest
 
 import run
 from run._utils import _find_duplicate_passages, _len_of_dups, split_dataset
-from run.data._loader import Alignment, make_en_speaker
+from run.data._loader import make_en_speaker
 from tests._utils import TEST_DATA_PATH
-from tests.run._utils import make_passage
+from tests.run._utils import make_alignments_1d, make_passage
 
 TEST_DATA_PATH = TEST_DATA_PATH / "audio"
 TEST_DATA_LJ = TEST_DATA_PATH / "bit(rate(lj_speech,24000),32).wav"
@@ -46,12 +46,11 @@ def test__find_duplicate_passages__no_duplicates():
 
 def test__len_of_dups():
     """Test `run._utils._len_of_dups` handles a basic case."""
-    alignments = Alignment.stow([Alignment((0, 1), (0, 1), (0, 1))])
     passages = [
-        make_passage(script="a", alignments=alignments),
-        make_passage(script="b", alignments=alignments),
-        make_passage(script="c", alignments=alignments),
-        make_passage(script="a", alignments=alignments),
+        make_passage(script="a"),
+        make_passage(script="b"),
+        make_passage(script="c"),
+        make_passage(script="a"),
     ]
     assert _len_of_dups((0, passages[0]), passages, 1.0) == 2
     assert _len_of_dups((1, passages[1]), passages, 1.0) == 1
@@ -67,9 +66,8 @@ def test_split_dataset__deduplication(_):
     speaker_c = make_en_speaker("c")
     speaker_d = make_en_speaker("d")
     groups = [set([speaker_a, speaker_b, speaker_c, speaker_d])]
-    alignments = Alignment.stow([Alignment((0, 1), (0, 1), (0, 1))])
     passage = lambda script, speaker: make_passage(
-        script=script, speaker=speaker, alignments=alignments
+        script=script, speaker=speaker, alignments=make_alignments_1d([(0, 1)])
     )
     dev_speakers = set([speaker_a, speaker_b, speaker_c])
     dev_length = 1
@@ -127,9 +125,8 @@ def test_split_dataset__order(_):
     speaker_a = make_en_speaker("a")
     speaker_b = make_en_speaker("b")
     groups = [set([speaker_a, speaker_b])]
-    alignments = Alignment.stow([Alignment((0, 1), (0, 1), (0, 1))])
     passage = lambda script, speaker: make_passage(
-        script=script, speaker=speaker, alignments=alignments
+        script=script, speaker=speaker, alignments=make_alignments_1d([(0, 1)])
     )
     dev_speakers = set([speaker_a, speaker_b])
     dev_length = 1
@@ -159,9 +156,8 @@ def test_split_dataset__groups(_):
     speaker_a = make_en_speaker("a")
     speaker_b = make_en_speaker("b")
     groups = [{speaker_a}, {speaker_b}]
-    alignments = Alignment.stow([Alignment((0, 1), (0, 1), (0, 1))])
     passage = lambda script, speaker: make_passage(
-        script=script, speaker=speaker, alignments=alignments
+        script=script, speaker=speaker, alignments=make_alignments_1d([(0, 1)])
     )
     dev_speakers = set([speaker_a, speaker_b])
     dev_length = 1
