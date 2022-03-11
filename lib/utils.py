4f0cd29334a18b228d13a8f2fe05306598c910b4
Michael Petrochuk <petrochukm@gmail.com>
2022-03-11 08:10:13 -0800

    Slightly increase performance of _tok_to_idx

--- lib/utils.py
+++ lib/utils.py
@@ -572,8 +572,9 @@ class PaddingAndLazyEmbedding(torch.nn.Module):
             KeyError: Iff the module is in evaluation mode and "unknown token" is disabled this
                 will error iff `token` isn't in `self.vocab`.
         """
-        if self.training:
-            assert token in self.vocab or token in self._new_tokens, "Invariant failure."
+        # NOTE: Useful invariant for debugging
+        # if self.training:
+        #     assert token in self.vocab or token in self._new_tokens, "Invariant failure."
 
         if not self.training and not self.allow_unk_on_eval and token not in self.vocab:
             raise KeyError(f"Token not found: {token}")
@@ -583,10 +584,12 @@ class PaddingAndLazyEmbedding(torch.nn.Module):
             # NOTE: Track unknown tokens so that they are not logged over and over.
             self._unk_tokens.add(token)
 
-        if self.training and token not in self.vocab:
+        idx = self.vocab.get(token, self.unk_idx)
+
+        if self.training and idx is self.unk_idx:
             logger.info(f"[Training] Using unknown token in-place of '{token}'.")
 
-        return self.vocab.get(token, self.unk_idx)
+        return idx
 
     def _should_update_proactively(self):
         return (
