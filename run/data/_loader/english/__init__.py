bc3f2bd7f14c2cb25172ec9b17cf99218c1cf364
Michael Petrochuk <petrochukm@gmail.com>
2022-03-26 11:49:37 -0700

    Move lj speech to the right folder

--- run/data/_loader/english/__init__.py
+++ run/data/_loader/english/__init__.py
@@ -2,6 +2,7 @@ import typing
 
 from run.data._loader.data_structures import Speaker
 from run.data._loader.english import m_ailabs, wsl, wsl_archive
+from run.data._loader.english.lj_speech import LINDA_JOHNSON, lj_speech_dataset
 from run.data._loader.english.m_ailabs import (
     ELIZABETH_KLETT,
     ELLIOT_MILLER,
@@ -88,14 +89,13 @@ from run.data._loader.english.wsl_archive import (
     LINCOLN__CUSTOM,
     OLD_WSL_DATASETS,
 )
-from run.data._loader.lj_speech import LINDA_JOHNSON, lj_speech_dataset
 from run.data._loader.utils import DataLoader
 
 # TODO: Consider updating M-AILABS and LJSpeech to Google Storage, so that we can download
 # and upload them faster. It'll also give us protection, if the datasets are deleted.
 
 DATASETS = typing.cast(typing.Dict[Speaker, DataLoader], WSL_DATASETS.copy())
-DATASETS[LINDA_JOHNSON] = lj_speech_dataset  # type: ignore
+DATASETS[LINDA_JOHNSON] = lj_speech_dataset
 DATASETS[JUDY_BIEBER] = m_ailabs_en_us_judy_bieber_speech_dataset
 DATASETS[MARY_ANN] = m_ailabs_en_us_mary_ann_speech_dataset
 DATASETS[ELLIOT_MILLER] = m_ailabs_en_us_elliot_miller_speech_dataset
@@ -105,6 +105,8 @@ __all__ = [
     "m_ailabs",
     "wsl",
     "wsl_archive",
+    "LINDA_JOHNSON",
+    "lj_speech_dataset",
     "ELIZABETH_KLETT",
     "ELLIOT_MILLER",
     "JUDY_BIEBER",
