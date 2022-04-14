+ Use correct naming conventions for typing + Handle pronunciation for various spaCy tokens + Rename `spacy_with_context` to `spacy_context` for brevity + Allow schwa through text normalization + Rewrite input preprocessing to allow for tokens to be rewritten + Add checks for respelled text + Update casing to include the pronunciation

run/_models/spectrogram_model/inputs.py
@@ -1,14 +1,24 @@
 import dataclasses
 import enum
+import random
+import string
 import typing
 
 import config as cf
+import numpy as np
 import spacy
 import spacy.tokens
 import torch
 
+from lib.text import get_respelling, load_cmudict_syl
 from lib.utils import lengths_to_mask
 from run.data._loader import structures as struc
+
+
+class Pronunciation(enum.Enum):
+
+    NORMAL: typing.Final = "normal"
+    RESPELLING: typing.Final = "respelling"
 
 
 class Casing(enum.Enum):
@@ -26,11 +36,84 @@ class Context(enum.Enum):
     SCRIPT: typing.Final = "script"
 
 
-def _get_case(c: str) -> Casing:
-    assert len(c) == 1
-    if c.isupper():
-        return Casing.UPPER
-    return Casing.LOWER if c.islower() else Casing.NO_CASING
+@dataclasses.dataclass(frozen=True)
+class Token:
+    token: spacy.tokens.token.Token
+    is_context: bool
+    is_whitespace: bool
+    try_to_respell: bool
+    # TODO: Should these be added to configuration?
+    # NOTE: These prefixes and suffixes are choosen based on spaCy's tokenizer. This ensures that
+    # |\\mother\\in\\law\\|" is treated as one token instead of being tokenized.
+    prefix: str = dataclasses.field(default="|\\", init=False, repr=False)
+    suffix: str = dataclasses.field(default="\\|", init=False, repr=False)
+    delim: str = dataclasses.field(default="\\", init=False, repr=False)
+    text: str = dataclasses.field(init=False, repr=False)
+    pronun: Pronunciation = dataclasses.field(init=False, repr=False)
+
+    def __post_init__(self):
+        if not self._is_respelled():
+            assert self.prefix not in self.token.text, "`prefix` must be unique"
+            assert self.suffix not in self.token.text, "`suffix` must be unique"
+        pronun, text = self._get_text()
+        object.__setattr__(self, "text", text)
+        object.__setattr__(self, "pronun", pronun)
+
+    def _is_respelled(self):
+        """Is the `self.token.text` already respelled?"""
+        text = self.token.text
+        is_respelled = text.startswith(self.prefix) and text.endswith(self.suffix)
+        text = text[len(self.prefix) : -len(self.suffix)]
+        if is_respelled:
+            valid_letters = string.ascii_lowercase + "ə"
+            message = "Invalid respelling."
+            assert len(text) > 0, message
+            assert text[0].lower() in valid_letters, message
+            assert text[-1].lower() in valid_letters, message
+            assert len(set(text.lower()) - set(list(valid_letters + self.delim))) == 0, message
+        return is_respelled
+
+    def _try_respelling(self) -> typing.Optional[str]:
+        """Get a respelling of `self.token.text` if possible."""
+        respell = get_respelling(self.token.text, load_cmudict_syl(), self.delim)
+        return None if respell is None else respell
+
+    def _get_text(self) -> typing.Tuple[Pronunciation, str]:
+        """Get the text that represents `token` which maybe respelled."""
+        if self.is_whitespace:
+            return (Pronunciation.NORMAL, self.token.whitespace_)
+        if self._is_respelled():
+            return (Pronunciation.RESPELLING, self.token.text[len(self.prefix) : -len(self.suffix)])
+        if self.try_to_respell:
+            repselling = self._try_respelling()
+            if repselling is not None:
+                return (Pronunciation.RESPELLING, repselling)
+        return (Pronunciation.NORMAL, self.token.text)
+
+    @property
+    def embed(self):
+        """Get an embedding for this token."""
+        assert self.token.tensor is not None
+        # TODO: At the moment, if a pronunciation exists, then we don't have data on the original
+        # word and it's meaning. In the future, when the original word is not lost
+        # we can preserve the original word vector.
+        if self.pronun is Pronunciation.RESPELLING or self.is_whitespace:
+            return np.zeros(self.token.tensor.shape[0] + self.token.vector.shape[0])
+        return np.concatenate((self.token.vector, self.token.tensor))
+
+    @staticmethod
+    def _get_case(c: str) -> Casing:
+        assert len(c) == 1
+        if c.isupper():
+            return Casing.UPPER
+        return Casing.LOWER if c.islower() else Casing.NO_CASING
+
+    @property
+    def casing(self):
+        return [(self.pronun, Token._get_case(c)) for c in self.text]
+
+    def __len__(self):
+        return len(self.text)
 
 
 @dataclasses.dataclass(frozen=True)
@@ -76,69 +159,85 @@ class Inputs:
     def reconstruct_text(self, i: int) -> str:
         """Reconstruct text from uncased text, and casing labels."""
         text = typing.cast(typing.List[str], self.tokens[i])
-        casing = typing.cast(typing.List[Casing], self.token_metadata[0][i])
-        return "".join([t.upper() if c == Casing.UPPER else t for t, c in zip(text, casing)])
+        casing = typing.cast(
+            typing.List[typing.Tuple[Pronunciation, Casing]], self.token_metadata[0][i]
+        )
+        return "".join([t.upper() if c == Casing.UPPER else t for t, (_, c) in zip(text, casing)])
 
 
-def _make_token_embeddings(
-    span: typing.Union[spacy.tokens.span.Span, spacy.tokens.doc.Doc], device: torch.device
-) -> torch.Tensor:
-    """Make a `Tensor` that is an embedding of the `span`."""
-    # TODO: Instead of using `zeros`, what if we tried training a vector, instead?
-    span = span[:] if isinstance(span, spacy.tokens.doc.Doc) else span
-    embeddings = torch.zeros(len(str(span)), span.doc.vector.size, device=device)
-    for word in span:
-        word_embedding = torch.tensor(word.vector, device=device).unsqueeze(0).repeat(len(word), 1)
-        idx = word.idx - span.start_char
-        embeddings[idx : idx + len(word)] = word_embedding
-    return embeddings
+SpanDoc = typing.Union[spacy.tokens.span.Span, spacy.tokens.doc.Doc]
 
 
-def _append_tokens_and_metadata(
-    inputs: Inputs,
-    span: typing.Union[spacy.tokens.span.Span, spacy.tokens.doc.Doc],
-    start_char: int,
-    end_char: int,
-):
-    """Preprocess and append `tokens` and `token_metadata` to `inputs`."""
-    text = str(span)
-    inputs.tokens.append(list(text.lower()))
-    inputs.slices.append(slice(start_char, end_char))
-    inputs.token_metadata[0].append([_get_case(c) for c in text])
-    inputs.token_metadata[1].append([Context.CONTEXT for _ in text])
-    for i in range(*inputs.slices[-1].indices(len(text))):
-        inputs.token_metadata[1][-1][i] = Context.SCRIPT
+def _token_to_tokens(
+    token: spacy.tokens.token.Token, span: SpanDoc, respell_prob: float
+) -> typing.Tuple[Token, Token]:
+    """Convert `token` into `Token`s."""
+    try_to_respell = random.random() < respell_prob
+
+    last_token = None if token.i == 0 else token.doc[token.i - 1]
+    if last_token is not None and len(last_token.whitespace_) == 0 and not last_token.is_punct:
+        try_to_respell = False
+
+    next_token = None if len(token.doc) - 1 == token.i else token.doc[token.i + 1]
+    if next_token is not None and len(token.whitespace_) == 0 and not next_token.is_punct:
+        try_to_respell = False
+
+    is_context = token not in span
+    is_whitespace_context = True if token == span[-1] else is_context
+    return (
+        Token(token, is_context, False, try_to_respell),
+        Token(token, is_whitespace_context, True, False),
+    )
 
 
-def _append_seq_metadata(seq_metadata: typing.List, session: struc.Session):
-    """Add metadata about the sequence to the model.
-
-    TODO: Create a named tuple to organize `seq_metadata` instead of referencing indicies.
-    """
-    seq_metadata[0].append(session[0].label)
-    seq_metadata[1].append(session)
-    seq_metadata[2].append(session[0].dialect)
-    seq_metadata[3].append(session[0].style)
-    seq_metadata[4].append(session[0].language)
-
-
-def _make_inputs(device: torch.device):
-    return Inputs([], [[], [], [], [], []], [[], []], [], [], device)
-
-
-def preprocess_spans(
-    spans: typing.List[struc.Span], device: torch.device = torch.device("cpu")
+def _preprocess(
+    batch: typing.List[typing.Tuple[struc.Session, SpanDoc, SpanDoc]],
+    device: torch.device = torch.device("cpu"),
+    respell_prob: float = 0.0,
 ) -> Inputs:
-    """Preprocess inputs to inputs by including casing, context, and embeddings."""
-    return_ = _make_inputs(device)
-    for span in spans:
-        context = span.spacy_with_context(**cf.get())
-        start_char = span.spacy.start_char - context.start_char
-        _append_seq_metadata(return_.seq_metadata, span.session)
-        _append_tokens_and_metadata(return_, context, start_char, start_char + len(str(span.spacy)))
-        typing.cast(list, return_.token_embeddings).append(_make_token_embeddings(context, device))
-    token_embeddings = torch.nn.utils.rnn.pad_sequence(return_.token_embeddings, batch_first=True)
-    return dataclasses.replace(return_, token_embeddings=token_embeddings)
+    """Preprocess `batch` into model `Inputs`.
+
+    NOTE: Preprocess as much as possible here so the model is as fast as possible.
+
+    TODO: Instead of using `zero` embeddings, what if we tried training a vector, instead?
+    """
+    inputs = Inputs([], [[], [], [], [], []], [[], []], [], [], device)
+    for sesh, context_span, span in batch:
+        tokens = [t for token in context_span for t in _token_to_tokens(token, span, respell_prob)]
+
+        seq_metadata = (sesh[0].label, sesh, sesh[0].dialect, sesh[0].style, sesh[0].language)
+        for i, data in enumerate(seq_metadata):
+            inputs.seq_metadata[i].append(data)
+
+        if len(tokens) > 0:
+            start_index = next(i for i, t in enumerate(tokens) if not t.is_context)
+            start_char = sum(len(t.text) for t in tokens[:start_index])
+            end_char = start_char + sum(len(t) for t in tokens if not t.is_context)
+        else:
+            start_char, end_char = 0, 0
+        inputs.slices.append(slice(start_char, end_char))
+
+        chars = [c for t in tokens for c in t.text]
+        inputs.tokens.append([c.lower() for c in chars])
+        inputs.token_metadata[0].append([c for t in tokens for c in t.casing])
+        inputs.token_metadata[1].append([Context.CONTEXT for _ in chars])
+        for i in range(*inputs.slices[-1].indices(len(chars))):
+            inputs.token_metadata[1][-1][i] = Context.SCRIPT
+
+        if len(tokens) == 0:
+            embed = torch.zeros(0, 0, device=device)
+        else:
+            embed = [torch.tensor(t.embed, device=device, dtype=torch.float32) for t in tokens]
+            embed = [e.unsqueeze(0).repeat(len(t), 1) for e, t in zip(embed, tokens)]
+            embed = torch.cat(embed)
+        typing.cast(list, inputs.token_embeddings).append(embed)
+
+    token_embeddings = torch.nn.utils.rnn.pad_sequence(inputs.token_embeddings, batch_first=True)
+    return dataclasses.replace(inputs, token_embeddings=token_embeddings)
+
+
+def preprocess_spans(spans: typing.List[struc.Span], **kw) -> Inputs:
+    return _preprocess([(s.session, cf.partial(s.spacy_context)(), s.spacy) for s in spans], **kw)
 
 
 class InputsWrapper(typing.NamedTuple):
@@ -151,12 +250,5 @@ class InputsWrapper(typing.NamedTuple):
     doc: typing.List[spacy.tokens.doc.Doc]
 
 
-def preprocess_inputs(inputs: InputsWrapper, device: torch.device = torch.device("cpu")) -> Inputs:
-    """Preprocess inputs to inputs by including casing, context, and embeddings."""
-    return_ = _make_inputs(device)
-    for doc, sesh in zip(inputs.doc, inputs.session):
-        _append_seq_metadata(return_.seq_metadata, sesh)
-        _append_tokens_and_metadata(return_, doc, 0, len(str(doc)))
-        typing.cast(list, return_.token_embeddings).append(_make_token_embeddings(doc, device))
-    token_embeddings = torch.nn.utils.rnn.pad_sequence(return_.token_embeddings, batch_first=True)
-    return dataclasses.replace(return_, token_embeddings=token_embeddings)
+def preprocess_inputs(inputs: InputsWrapper, **kw) -> Inputs:
+    return _preprocess([(s, d, d) for s, d in zip(inputs.session, inputs.doc)], **kw)

tests/run/_models/spectrogram_model/test_inputs.py
@@ -5,14 +5,17 @@ import torch
 
 import run
 from run._config import load_spacy_nlp
-from run._models.spectrogram_model.wrapper import (
+from run._models.spectrogram_model.inputs import (
     Casing,
+    Context,
     InputsWrapper,
+    Pronunciation,
+    _preprocess,
     preprocess_inputs,
     preprocess_spans,
 )
 from run.data._loader.structures import Language
-from tests.run._utils import make_passage
+from tests.run._utils import make_passage, make_session
 
 
 @pytest.fixture(autouse=True)
@@ -23,56 +26,232 @@ def run_around_tests():
     cf.purge()
 
 
-def test_preprocess_spans():
-    """Test that `preprocess_spans` handles a basic input."""
+def test__preprocess():
+    """Test that `_preprocess` handles a basic input."""
     nlp = load_spacy_nlp(Language.ENGLISH)
-    script = "In 1968 the U.S. Army"
+    script, sesh = "In 1968 the U.S. Army", make_session()
     doc = nlp(script)
-    passage = make_passage(script=script)
-    processed = preprocess_spans([passage[:-1]])
+    processed = _preprocess([(sesh, doc, doc[:-1])])
     assert processed.reconstruct_text(0) == script
     assert processed.tokens == [list(script.lower())]
-    assert processed.seq_metadata == [[passage.speaker], [passage.session]]
+    assert processed.seq_metadata[0] == [sesh[0].label]
+    assert processed.seq_metadata[1] == [sesh]
+    assert script[processed.slices[0]] == str(doc[:-1])
     casing = [
-        Casing.UPPER,  # I
-        Casing.LOWER,  # n
-        Casing.NO_CASING,
-        Casing.NO_CASING,  # 1
-        Casing.NO_CASING,  # 9
-        Casing.NO_CASING,  # 6
-        Casing.NO_CASING,  # 8
-        Casing.NO_CASING,
-        Casing.LOWER,  # t
-        Casing.LOWER,  # h
-        Casing.LOWER,  # e
-        Casing.NO_CASING,
-        Casing.UPPER,  # u
-        Casing.NO_CASING,  # .
-        Casing.UPPER,  # s
-        Casing.NO_CASING,  # .
-        Casing.NO_CASING,
-        Casing.UPPER,  # A
-        Casing.LOWER,  # r
-        Casing.LOWER,  # m
-        Casing.LOWER,  # y
+        (Pronunciation.NORMAL, Casing.UPPER),  # I
+        (Pronunciation.NORMAL, Casing.LOWER),  # n
+        (Pronunciation.NORMAL, Casing.NO_CASING),
+        (Pronunciation.NORMAL, Casing.NO_CASING),  # 1
+        (Pronunciation.NORMAL, Casing.NO_CASING),  # 9
+        (Pronunciation.NORMAL, Casing.NO_CASING),  # 6
+        (Pronunciation.NORMAL, Casing.NO_CASING),  # 8
+        (Pronunciation.NORMAL, Casing.NO_CASING),
+        (Pronunciation.NORMAL, Casing.LOWER),  # t
+        (Pronunciation.NORMAL, Casing.LOWER),  # h
+        (Pronunciation.NORMAL, Casing.LOWER),  # e
+        (Pronunciation.NORMAL, Casing.NO_CASING),
+        (Pronunciation.NORMAL, Casing.UPPER),  # u
+        (Pronunciation.NORMAL, Casing.NO_CASING),  # .
+        (Pronunciation.NORMAL, Casing.UPPER),  # s
+        (Pronunciation.NORMAL, Casing.NO_CASING),  # .
+        (Pronunciation.NORMAL, Casing.NO_CASING),
+        (Pronunciation.NORMAL, Casing.UPPER),  # A
+        (Pronunciation.NORMAL, Casing.LOWER),  # r
+        (Pronunciation.NORMAL, Casing.LOWER),  # m
+        (Pronunciation.NORMAL, Casing.LOWER),  # y
     ]
-    assert processed.token_metadata == [[(c,) for c in casing]]
+    assert processed.token_metadata[0] == [casing]
+    context = [
+        Context.SCRIPT,  # I
+        Context.SCRIPT,  # n
+        Context.SCRIPT,
+        Context.SCRIPT,  # 1
+        Context.SCRIPT,  # 9
+        Context.SCRIPT,  # 6
+        Context.SCRIPT,  # 8
+        Context.SCRIPT,
+        Context.SCRIPT,  # t
+        Context.SCRIPT,  # h
+        Context.SCRIPT,  # e
+        Context.SCRIPT,
+        Context.SCRIPT,  # u
+        Context.SCRIPT,  # .
+        Context.SCRIPT,  # s
+        Context.SCRIPT,  # .
+        Context.CONTEXT,
+        Context.CONTEXT,  # A
+        Context.CONTEXT,  # r
+        Context.CONTEXT,  # m
+        Context.CONTEXT,  # y
+    ]
+    assert processed.token_metadata[1] == [context]
     vocab: spacy.vocab.Vocab = nlp.vocab
+    length = nlp.meta["vectors"]["width"]
     embeddings = [
         torch.from_numpy(vocab["in"].vector).unsqueeze(0).repeat(2, 1),
-        torch.zeros(1, nlp.meta["vectors"]["width"]),
+        torch.zeros(1, length),
         torch.from_numpy(vocab["1968"].vector).unsqueeze(0).repeat(4, 1),
-        torch.zeros(1, nlp.meta["vectors"]["width"]),
+        torch.zeros(1, length),
         torch.from_numpy(vocab["the"].vector).unsqueeze(0).repeat(3, 1),
-        torch.zeros(1, nlp.meta["vectors"]["width"]),
+        torch.zeros(1, length),
         torch.from_numpy(vocab["u.s."].vector).unsqueeze(0).repeat(4, 1),
-        torch.zeros(1, nlp.meta["vectors"]["width"]),
+        torch.zeros(1, length),
         torch.from_numpy(vocab["army"].vector).unsqueeze(0).repeat(4, 1),
     ]
     token_embeddings = torch.cat(embeddings)
     assert len(processed.token_embeddings) == 1
-    assert torch.allclose(processed.token_embeddings[0], token_embeddings)
-    assert processed.slices == [slice(0, len(doc.text) - 5)]
+    assert torch.allclose(processed.token_embeddings[0][:, :length], token_embeddings)
+
+
+def test__preprocess_respelling():
+    """Test that `_preprocess` handles apostrophes, dashes, initialisms and existing respellings."""
+    nlp = load_spacy_nlp(Language.ENGLISH)
+    script, sesh = "Don't |\\PEE\\puhl\\| from EDGE catch-the-flu?", make_session()
+    doc = nlp(script)
+    processed = _preprocess([(sesh, doc, doc[1:-1])], respell_prob=1.0)
+    casing = [
+        (Pronunciation.NORMAL, Casing.UPPER),  # D
+        (Pronunciation.NORMAL, Casing.LOWER),  # o
+        (Pronunciation.NORMAL, Casing.LOWER),  # n
+        (Pronunciation.NORMAL, Casing.NO_CASING),  # '
+        (Pronunciation.NORMAL, Casing.LOWER),  # t
+        (Pronunciation.NORMAL, Casing.NO_CASING),
+        (Pronunciation.RESPELLING, Casing.UPPER),  # P
+        (Pronunciation.RESPELLING, Casing.UPPER),  # E
+        (Pronunciation.RESPELLING, Casing.UPPER),  # E
+        (Pronunciation.RESPELLING, Casing.NO_CASING),  # \
+        (Pronunciation.RESPELLING, Casing.LOWER),  # p
+        (Pronunciation.RESPELLING, Casing.LOWER),  # u
+        (Pronunciation.RESPELLING, Casing.LOWER),  # h
+        (Pronunciation.RESPELLING, Casing.LOWER),  # l
+        (Pronunciation.NORMAL, Casing.NO_CASING),
+        (Pronunciation.NORMAL, Casing.LOWER),  # f
+        (Pronunciation.NORMAL, Casing.LOWER),  # r
+        (Pronunciation.NORMAL, Casing.LOWER),  # o
+        (Pronunciation.NORMAL, Casing.LOWER),  # m
+        (Pronunciation.NORMAL, Casing.NO_CASING),
+        (Pronunciation.NORMAL, Casing.UPPER),  # E
+        (Pronunciation.NORMAL, Casing.UPPER),  # D
+        (Pronunciation.NORMAL, Casing.UPPER),  # G
+        (Pronunciation.NORMAL, Casing.UPPER),  # E
+        (Pronunciation.NORMAL, Casing.NO_CASING),
+        (Pronunciation.RESPELLING, Casing.UPPER),  # K
+        (Pronunciation.RESPELLING, Casing.UPPER),  # A
+        (Pronunciation.RESPELLING, Casing.UPPER),  # T
+        (Pronunciation.RESPELLING, Casing.UPPER),  # C
+        (Pronunciation.RESPELLING, Casing.UPPER),  # H
+        (Pronunciation.NORMAL, Casing.NO_CASING),
+        (Pronunciation.NORMAL, Casing.LOWER),  # t
+        (Pronunciation.NORMAL, Casing.LOWER),  # h
+        (Pronunciation.NORMAL, Casing.LOWER),  # e
+        (Pronunciation.NORMAL, Casing.NO_CASING),
+        (Pronunciation.RESPELLING, Casing.UPPER),  # F
+        (Pronunciation.RESPELLING, Casing.UPPER),  # L
+        (Pronunciation.RESPELLING, Casing.UPPER),  # O
+        (Pronunciation.RESPELLING, Casing.UPPER),  # O
+        (Pronunciation.NORMAL, Casing.NO_CASING),  # ?
+    ]
+    assert processed.token_metadata[0] == [casing]
+    assert processed.reconstruct_text(0) == "Don't PEE\\puhl from EDGE KATCH-the-FLOO?"
+    tokens = "n't pee\\puhl from edge katch-the-floo"
+    assert "".join(processed.tokens[0][processed.slices[0]]) == tokens  # type: ignore
+    context = [
+        Context.CONTEXT,  # D
+        Context.CONTEXT,  # o
+        Context.SCRIPT,  # n
+        Context.SCRIPT,  # '
+        Context.SCRIPT,  # t
+        Context.SCRIPT,
+        Context.SCRIPT,  # P
+        Context.SCRIPT,  # E
+        Context.SCRIPT,  # E
+        Context.SCRIPT,  # \
+        Context.SCRIPT,  # p
+        Context.SCRIPT,  # u
+        Context.SCRIPT,  # h
+        Context.SCRIPT,  # l
+        Context.SCRIPT,
+        Context.SCRIPT,  # f
+        Context.SCRIPT,  # r
+        Context.SCRIPT,  # o
+        Context.SCRIPT,  # m
+        Context.SCRIPT,
+        Context.SCRIPT,  # E
+        Context.SCRIPT,  # D
+        Context.SCRIPT,  # G
+        Context.SCRIPT,  # E
+        Context.SCRIPT,
+        Context.SCRIPT,  # K
+        Context.SCRIPT,  # A
+        Context.SCRIPT,  # T
+        Context.SCRIPT,  # C
+        Context.SCRIPT,  # H
+        Context.SCRIPT,
+        Context.SCRIPT,  # t
+        Context.SCRIPT,  # h
+        Context.SCRIPT,  # e
+        Context.SCRIPT,
+        Context.SCRIPT,  # F
+        Context.SCRIPT,  # L
+        Context.SCRIPT,  # O
+        Context.SCRIPT,  # O
+        Context.CONTEXT,  # ?
+    ]
+    assert processed.token_metadata[1] == [context]
+    assert processed.token_embeddings[0][-5:-1].sum().item() == 0.0  # FLOO
+    assert processed.token_embeddings[0][:3].sum().item() != 0.0  # n't
+
+
+def test__preprocess_zero_length():
+    """Test that `_preprocess` handles a zero length script."""
+    nlp = load_spacy_nlp(Language.ENGLISH)
+    script, sesh = "", make_session()
+    doc = nlp(script)
+    processed = _preprocess([(sesh, doc, doc)], respell_prob=1.0)
+    assert processed.reconstruct_text(0) == ""
+    assert "".join(processed.tokens[0][processed.slices[0]]) == ""  # type: ignore
+    assert processed.token_metadata[0] == [[]]
+    assert processed.token_metadata[1] == [[]]
+    assert processed.slices[0] == slice(0, 0)
+    assert processed.token_embeddings[0].shape == (0, 0)
+
+
+def test__preprocess_schwa():
+    """Test that `_preprocess` handles the special character schwa."""
+    nlp = load_spacy_nlp(Language.ENGLISH)
+    script, sesh = "motorcycle", make_session()
+    doc = nlp(script)
+    processed = _preprocess([(sesh, doc, doc)], respell_prob=1.0)
+    assert processed.reconstruct_text(0) == "MOH\\tər\\sy\\kuhl"
+
+
+def test__preprocess_invalid_respelling():
+    """Test that `_preprocess` handles errors if respelling is invalid."""
+    nlp = load_spacy_nlp(Language.ENGLISH)
+
+    with pytest.raises(AssertionError):
+        doc = nlp("|\\\\|")  # Zero length
+        _preprocess([(make_session(), doc, doc)])
+
+    with pytest.raises(AssertionError):
+        doc = nlp("|\\\\MOH\\|")  # Invalid prefix
+        _preprocess([(make_session(), doc, doc)])
+
+    with pytest.raises(AssertionError):
+        doc = nlp("|\\MOH\\\\|")  # Invalid suffix
+        _preprocess([(make_session(), doc, doc)])
+
+    with pytest.raises(AssertionError):
+        doc = nlp("|\\MOH tər\\|")  # Invalid character
+        _preprocess([(make_session(), doc, doc)])
+
+    with pytest.raises(AssertionError):
+        doc = nlp("|\\MOH5tər\\|")  # Invalid character
+        _preprocess([(make_session(), doc, doc)])
+
+    with pytest.raises(AssertionError):
+        doc = nlp("|\\MOH|tər\\|")  # Invalid character
+        _preprocess([(make_session(), doc, doc)])
 
 
 def test_preprocess_inputs_and_spans():
@@ -81,12 +260,15 @@ def test_preprocess_inputs_and_spans():
     script = "In 1968 the U.S. Army"
     passage = make_passage(script=script)
     pre_span = preprocess_spans([passage[1:-1]])
-    pre_doc = preprocess_inputs(
-        InputsWrapper(session=[passage.session], doc=[nlp(passage[1:-1].script)])
-    )
+    inputs = InputsWrapper(session=[passage.session], doc=[nlp(passage[1:-1].script)])
+    pre_doc = preprocess_inputs(inputs)
     assert pre_doc.seq_metadata == pre_span.seq_metadata
     assert pre_doc.tokens[0] == pre_span.tokens[0][3:-5]
     assert pre_doc.token_metadata == [[s[3:-5] for s in m] for m in pre_span.token_metadata]
-    assert torch.allclose(pre_doc.token_embeddings[0], pre_span.token_embeddings[0][3:-5])
+    length = nlp.meta["vectors"]["width"]
+    assert torch.allclose(
+        pre_doc.token_embeddings[0][:, :length],
+        pre_span.token_embeddings[0][3:-5][:, :length],
+    )
     doc_slice, span_slice = pre_doc.slices[0], pre_span.slices[0]
     assert doc_slice.stop - doc_slice.start == span_slice.stop - span_slice.start