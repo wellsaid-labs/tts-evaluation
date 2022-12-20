a0cbc5671bab3a81a3f2234e7d9c2d5c5eb8da89
Michael Petrochuk <michael@wellsaidlabs.com>
2022-12-20 13:08:10 -0800

    + Fix config + Fix test

--- run/train/spectrogram_model/_data.py
+++ run/train/spectrogram_model/_data.py
@@ -353,7 +353,7 @@ def make_batch(spans: typing.List[Span], max_workers: int = 6) -> Batch:
         span=[s.spacy for s in spans],
         context=[cf.partial(s.spacy_context)() for s in spans],
         loudness=[_random_loudness_annotations(s, a) for s, a in zip(spans, signals_)],
-        tempo=[cf.partial(_random_tempo_annotations)(s) for s in spans],
+        tempo=[_random_tempo_annotations(s) for s in spans],
         respellings=[cf.partial(_random_respelling_annotations)(s) for s in spans],
     )
     # NOTE: `inputs` has a spaCy `Span` which is difficult to `pickle`, so instead, we seralize
