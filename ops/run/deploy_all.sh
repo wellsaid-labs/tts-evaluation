a5c2dc2f96e64f9173479eac3ac2a9a1e68e97bb
Sam Skjonsberg <sams@allenai.org>
2023-01-05 23:17:17 -0800

    Add script for deploying all models

---
+++ ops/run/deploy_all.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#
+# This script deploys all of the configurations for a particular environment.
+# It should be used carefully because of it's breadth.
+#
+
+set -euo pipefail
+
+if [[ -z "$1" ]]; then
+    echo "Error: must specify path to configuration files"
+    echo "Usage: deploy_all.sh PATH"
+    exit 1
+fi
+
+for conf in "$1"*.json; do
+    echo "Updating $conf..."
+    echo
+
+    dir=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+    "$dir"/deploy.sh "$conf"
+done
+
+echo
+echo "OK"
