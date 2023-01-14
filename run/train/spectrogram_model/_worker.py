1a85a1fca1d029798021b6cf00d63e43e0d9cb46
Michael Petrochuk <michael@wellsaidlabs.com>
2023-01-14 15:05:34 -0800

    Remove pickers

--- run/train/spectrogram_model/_worker.py
+++ run/train/spectrogram_model/_worker.py
@@ -7,7 +7,6 @@ import logging
 import math
 import pathlib
 import random
-import types
 import typing
 from functools import partial
 
@@ -48,7 +47,6 @@ from run.train.spectrogram_model._metrics import (
     Metrics,
     MetricsKey,
     MetricsValues,
-    get_alignment_norm,
     get_average_db_rms_level,
 )
 
@@ -303,11 +301,7 @@ class _HandleBatchArgs(typing.NamedTuple):
 
 
 def _visualize_source_vs_target(args: _HandleBatchArgs, preds: Preds):
-    """Visualize predictions as compared to the original `batch`.
-
-    TODO: Add `pick` so that we can find examples which perform poorly in training, and hence are
-    probably bad examples. Also, this should log the audio, for analysis.
-    """
+    """Visualize predictions as compared to the original `batch`."""
     if not is_master():
         return
 
@@ -446,29 +440,7 @@ def _run_step(
     args.metrics.update(values)
 
 
-def _min_alignment_norm(_: _HandleBatchArgs, preds: Preds) -> int:
-    """Get the index of the prediction that has the smallest alignment norm."""
-    return int(torch.argmin(get_alignment_norm(preds)))
-
-
-def _max_num_frames_diff(args: _HandleBatchArgs, preds: Preds) -> int:
-    """Get the index of the prediction that most deviates from the original spectrogram length."""
-    return int(torch.argmax((args.batch.spectrogram.lengths - preds.num_frames).abs()))
-
-
-def _random_sequence(args: _HandleBatchArgs, preds: Preds) -> int:
-    """Get a random batch index."""
-    return random.randint(0, len(args.batch) - 1)
-
-
-class _Pick(typing.Protocol):
-    """Get a batch index given the arguments and predictions."""
-
-    def __call__(self, args: _HandleBatchArgs, preds: Preds) -> int:
-        ...
-
-
-def _visualize_inferred(args: _HandleBatchArgs, preds: Preds, pick: _Pick = _random_sequence):
+def _visualize_inferred(args: _HandleBatchArgs, preds: Preds):
     """Run in inference mode and visualize results.
 
     TODO: Visualize any related text annotations.
@@ -476,8 +448,7 @@ def _visualize_inferred(args: _HandleBatchArgs, preds: Preds, pick: _Pick = _ran
     if not is_master():
         return
 
-    pick_label = typing.cast(types.MethodType, pick).__name__
-    item = pick(args, preds)
+    item = random.randint(0, len(args.batch) - 1)
     num_frames = int(args.batch.spectrogram.lengths[0, item].item())
     num_frames_predicted = int(preds.num_frames[item].item())
     text_length = int(preds.num_tokens[item].item())
@@ -488,10 +459,8 @@ def _visualize_inferred(args: _HandleBatchArgs, preds: Preds, pick: _Pick = _ran
     predicted_alignments = preds.alignments[:num_frames_predicted, item, :text_length]
     predicted_stop_token = preds.stop_tokens[:num_frames_predicted, item]
 
-    model = lambda n: get_model_label(f"{n}/{pick_label}", cadence=args.cadence)
-    dataset = lambda n: get_dataset_label(
-        f"{n}/{pick_label}", cadence=args.cadence, type_=args.dataset_type
-    )
+    model = lambda n: get_model_label(n, cadence=args.cadence)
+    dataset = lambda n: get_dataset_label(n, cadence=args.cadence, type_=args.dataset_type)
     _plot_mel_spectrogram = cf.partial(plot_mel_spectrogram)
     figures = (
         (dataset("gold_spectrogram"), _plot_mel_spectrogram, gold_spectrogram),
@@ -516,7 +485,6 @@ def _visualize_inferred(args: _HandleBatchArgs, preds: Preds, pick: _Pick = _ran
         session=args.batch.spans[item].session,
         predicted_loudness=get_average_db_rms_level(predicted_spectrogram.unsqueeze(1)).item(),
         gold_loudness=get_average_db_rms_level(gold_spectrogram.unsqueeze(1)).item(),
-        pick_function=pick_label,
         **{f"{k} Figure": link(v) for k, v in assets.items()},
         **{k: link(v) for k, v in npy_urls.items()},
     )
@@ -535,8 +503,7 @@ def _run_inference(args: _HandleBatchArgs):
 
     if args.visualize:
         args.timer.record_event(args.timer.VISUALIZE_PREDICTIONS)
-        for pick in [_random_sequence, _max_num_frames_diff, _min_alignment_norm]:
-            _visualize_inferred(args, preds, pick)
+        _visualize_inferred(args, preds)
 
     args.timer.record_event(args.timer.MEASURE_METRICS)
     values: MetricsValues = {
