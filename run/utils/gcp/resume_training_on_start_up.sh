16ca0504ed847c6573cf8c10cb971c9760981582
Michael Petrochuk <michael@wellsaidlabs.com>
2023-02-18 21:16:54 +0200

    Fix driver init

--- run/utils/gcp/resume_training_on_start_up.sh
+++ run/utils/gcp/resume_training_on_start_up.sh
@@ -20,6 +20,11 @@ if [ -z "$1" ]; then
 fi
 
 if [ -f /opt/wellsaid-labs/AUTO_START_FROM_CHECKPOINT ]; then
+  # NOTE: The drivers may rarely sometimes fail to initialize.
+  echo 'Reloading drivers, just in case...'
+  sudo rmmod nvidia_uvm
+  sudo modprobe nvidia_uvm
+
   echo 'Restarting from the latest checkpoint...'
   runuser -l $STARTUP_SCRIPT_USER -c "screen -dmL bash -c \
       'cd /opt/wellsaid-labs/Text-to-Speech;
