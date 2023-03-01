da5e13150eff87d8d4b26d616c2729bd2560c985
Michael Petrochuk <michael@wellsaidlabs.com>
2023-03-01 07:18:53 -0500

    + Add additional logs + Test specific speakers on various annotations + Add TODO + Fix spelling mistake

--- run/review/tts/test_cases.py
+++ run/review/tts/test_cases.py
@@ -35,7 +35,7 @@ from run._streamlit import (
 )
 from run._tts import griffin_lim_tts
 from run.data._loader import Speaker
-from run.data._loader.english.wsl import DIARMID_C
+from run.data._loader.english import wsl
 
 V10_REGRESSIONS = [
     # NOTE: Respellings are formatted like they were inputted in v10
@@ -197,7 +197,7 @@ def generate_test_cases(spec_export: SpectrogramModel, test_cases: typing.List[s
     vocab = list(spec_export.session_embed.get_vocab())
     for case in test_cases:
         sesh = random.choice(vocab)
-        st.info(f"Seshion: {sesh}")
+        st.info(f"Seshion: {sesh}\n\nScript: {case}")
         yield griffin_lim_tts(spec_export, XMLType(case), sesh)
 
 
@@ -205,19 +205,29 @@ Generator = typing.Callable[[SpectrogramModel], typing.Generator[np.ndarray, Non
 OPTIONS: typing.Dict[str, Generator]
 OPTIONS = {k: functools.partial(generate_test_cases, test_cases=v) for k, v in TEST_CASES.items()}
 
+# TODO: Create a notebook where we go through all the speakers, on various tempos/loudnesses, and
+# then review the accuracy.
+
 
 def generate_incremental_annotations(
     spec_export: SpectrogramModel,
-    speakers: typing.Set[Speaker] = {DIARMID_C},
-    annos: typing.Tuple[typing.Tuple[str, typing.Generator[float, None, None]], ...] = (
-        ("loudness", lib.utils.arange(-15, -35, -5)),
-        ("tempo", lib.utils.arange(0.5, 2.1, 0.1)),
+    speakers: typing.Sequence[Speaker] = [
+        wsl.TRISTAN_F,  # NOTE: They have a low annotation range around ±2db & ±25%.
+        wsl.GIA_V,  # NOTE: They have a low annotation range around ±2db & ±25%.
+        wsl.DIARMID_C,  # NOTE: They have a high annotation range around ±4db & ±45%.
+        wsl.GARRY_J__STORY,  # NOTE: They have a high annotation range around ±4db & ±45%.
+        # NOTE: They have an average range around ±3db & ±30% and have been difficult to work with.
+        wsl.JUDE_D__EN_GB,
+    ],
+    annos: typing.Tuple[typing.Tuple[str, typing.Sequence], ...] = (
+        ("loudness", list(lib.utils.arange(6, -9, -3))),
+        ("tempo", list(lib.utils.arange(0.5, 2.5, 0.25))),
     ),
 ):
     for speaker in speakers:
         sesh_vocab = spec_export.session_embed.get_vocab()
         sesh = random.choice([s for s in sesh_vocab if s.spkr == speaker])
-        st.info(f"Seshion: {sesh}")
+        st.info(f"Session: {sesh}")
         for tag, range in annos:
             for val in range:
                 xml = XMLType(f"<{tag} value='{val}'>{DEFAULT_SCRIPT}</{tag}>")
