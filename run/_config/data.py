6526a02882c64a3a4ce98f71e9b179ecb08fbe8d
Michael Petrochuk <michael@wellsaidlabs.com>
2023-04-11 21:59:16 -0700

    Decrease dev set requirements

--- run/_config/data.py
+++ run/_config/data.py
@@ -250,8 +250,8 @@ def configure(overwrite: bool = False):
         run._utils.split_dataset: cf.Args(
             groups=groups,
             dev_speakers=DEV_SPEAKERS,
-            approx_dev_len=30 * 60,
-            min_sim=0.9,
+            approx_dev_len=25 * 60,
+            min_sim=0.95,
             min_split_passages=3,
         ),
         run.data._loader.structures.Span.spacy_context: cf.Args(max_words=20),
