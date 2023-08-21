600e266017c28944fead88d8267acf63da0c1a46
Jordan Hoskins <jordan@Jordans-Laptop.local>
2023-08-21 14:58:13 -0600

    add test cases for slurring

---
+++ run/review/tts/_test_cases/slurring.py
@@ -0,0 +1,24 @@
+"""Scripts directly copied from a listening exercise conducted in August 2023 where participants
+noticed slurring. The scripts have not been altered in any way.
+"""
+SLURRING = [
+    """If you’ve ever cyberbullied someone, you may have thought it was funny at the time. When you can’t see someone’s face, it can be easier to be mean to them. But chances are, you don’t really want to cause anyone to experience anything on the list above. It’s important to use empathy for others, even when we’re online and can’t look them in the eyes. Empathy is when we put ourselves in another person’s shoes to identify with how they are feeling. The next time you’re thinking about being disrespectful online just to get a laugh, put yourself in the other person’s shoes. Ask yourself, would you like someone to treat you that way?
+    """,
+    """After a couple of minutes, Gaius groaned once more and opened his eyes.
+
+    “What… what happened?” he asked as he immediately tried to sit up.
+
+    Leon, pushing him back down, said, “We got the shadow cat, but you seemed to have passed out. How are you feeling?”
+
+    “Uggh, like I fought a bull and lost,” Gaius replied as he shifted around enough to pull his shirt up and see that the wound Leon had left on his abdomen had closed over with a freshly-healed skin. “At least that’s taken care of. Thank you, Leon.”
+    """,
+    """Hi, I'm Idris. I'm going to guide you through this session today. I'm so excited to have you here to learn about underwriting recreational vehicles for our customers! I'd like to get to know you before we start, can you enter your name please?
+    """,
+    """Let’s look at a couple of questions to reinforce what we have learned. You can pause the webcast to give yourself time to answer the question. Then, press Play to see-if you answered correctly and to hear the rationale. Keep in mind there may be more than one correct answer. Number 1. In an ::YY-see-DEE-TEHN:: PCS cabbage procedure code, what does the Body Part value identify? The name of the body part being bypassed. The number of coronary arteries being treated. The number of coronary arteries being bypassed to. The vessel bypassed from.
+    """,
+    """We're sorry for the delay. Our average wait time is well under a minute so we will be with you shortly., Did you know that we perform custom alterations for many of our customers? That’s right"".
+Do you need a shorter than standard inseam, but can't find that service elsewhere? All of our pants are hemmed on site free of charge, while adding a zipper to the leg of any of our pants can make accessing a knee brace or pross-thee-sis so much easier. Most alterations add only one day to your order turnaround, so ask your order taker for more information. We will be with you shortly.
+    """,
+    """It's time to take what you've learned about dockyousign; and put it into practice... In this course, you will learn how to access the dehmo environment; and have the opportunity to send practice documents... both with a templet and without a templet. It will take approximately 10 minutes to complete and the course does contain audio so please make sure your sound is turned on. To begin, click the start button.
+    """
+]
