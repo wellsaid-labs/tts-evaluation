fd8a5f0e94815cb0c655180911dcb111f7c9cfd2
Jordan Hoskins <jordan@Jordans-Laptop.local>
2023-08-22 11:26:59 -0600

    wrap option selection in form. add limit to number of sentences

--- run/review/tts/generate_audio_from_api.py
+++ run/review/tts/generate_audio_from_api.py
@@ -22,7 +22,9 @@ from run._streamlit import (
 
 test_case_options = V11_TEST_CASES
 test_case_options["SLURRING"] = SLURRING
-test_case_options = {k: v for k, v in sorted(test_case_options.items(), key=lambda x: x[0])}
+test_case_options = {
+    k: v for k, v in sorted(test_case_options.items(), key=lambda x: x[0])
+}
 
 staging_headers = {
     "X-Api-Key": "d5637035-23d6-472d-9b44-87001cd337dc",
@@ -43,7 +45,10 @@ previous_headers = {
 model_to_endpoints_and_headers = {
     "v9": ("https://api.wellsaidlabs.com/v1/tts/stream", previous_headers),
     "v10": ("https://api.wellsaidlabs.com/v1/tts/stream", prod_headers),
-    "v11": ("https://api.wellsaidlabs.com/v1/staging/tts/stream", staging_headers),
+    "v11": (
+        "https://api.wellsaidlabs.com/v1/staging/tts/stream",
+        staging_headers,
+    ),
 }
 
 speakers_in_v9_v10_v11 = {
@@ -83,94 +88,123 @@ def query_tts_api(speaker_id, text, style, headers, endpoint):
     return response
 
 
-def init_state():
-    st.session_state.download_paths = []
-
-
 def main():
     # Configure parameters and select appropriate headers for the API
     configure(overwrite=True)
-    if "download_paths" not in st.session_state:
-        init_state()
-    texts = []
-    endpoint, headers = None, None
-    model_version = st.selectbox(
-        label="Model Version", options=["v9", "v10", "v11"]
-    )
-    if model_version:
-        endpoint, headers = model_to_endpoints_and_headers[model_version]
-    test_case_selection = st.selectbox(
-        label="Text Cases", options=test_case_options
-    )
-    if test_case_selection and model_version:
-        texts = V11_TEST_CASES[test_case_selection]
-
-    # Generate audio
-    if all([st.button("Generate audio!"), model_version, test_case_selection]):
-        pbar = st.progress(0, text="Generating audio")
-        total_tasks = len(texts) * len(speakers_in_v9_v10_v11)
-        current_task = 0
-        metadata = []
-        for text in texts:
-            for speaker in speakers_in_v9_v10_v11:
-                speaker_id = speakers_in_v9_v10_v11[speaker]
-                audio_file_name = f"{model_version}_{speaker}_{text[:15]}.wav"
-                try:
-                    resp = query_tts_api(
-                        speaker_id=speaker_id,
-                        text=text,
-                        style=speaker,
-                        endpoint=endpoint,
-                        headers=headers,
-                    )
-                except requests.exceptions.ConnectionError:
-                    st.write("Connection error encountered, trying once more in 5s")
-                    time.sleep(5)
-                    resp = query_tts_api(
-                        speaker_id=speaker_id,
-                        text=text,
-                        style=speaker,
-                        endpoint=endpoint,
-                        headers=headers,
-                    )
-                if resp.status_code == 200:
-                    with tempfile.NamedTemporaryFile(mode="wb", suffix=".wav") as fp:
-                        fp.write(resp.content)
-                        fp.seek(0)
-                        audio_array, sample_rate = sf.read(fp.name, dtype="float32")
-                    path = audio_to_web_path(audio_array, name=audio_file_name)
-                    st.session_state.download_paths.append(path)
-                    file_data = {
-                        "Speaker": "".join(speaker.split("_")[0:1]),
-                        "Style": speaker.split("_")[-1],
-                        "Script": text,
-                        "Vote": [],
-                        "Note": "",
-                        "Session": "",
-                        "Dialect": "",
-                        "Audio": audio_file_name,
-                    }
-                    metadata.append(file_data)
-                else:
-                    msg = (
-                        f"Received status code {resp.status_code} while generating {audio_file_name}"
-                        f". Full content: {resp.content}"
-                    )
-                    st.write(msg)
-                current_task += 1
-                progress_pct = current_task / total_tasks
-                progress_txt = f"Generating audio - {round(progress_pct * 100, 2)}% complete"
-                pbar.progress(progress_pct, text=progress_txt)
-
-        # Create metadata csv and save zipped audio
-        metadata_path = make_temp_web_dir() / "metadata.csv"
-        metadata_path.write_text(pd.DataFrame(metadata).to_csv())
-        st.session_state.download_paths.append(metadata_path)
-        st_download_files(
-            f"{model_version}-{test_case_selection}-audio-and-metadata.zip",
-            "💾  DOWNLOAD  💾",
-            st.session_state.download_paths,
+    with st.form("Options"):
+        texts = []
+        endpoint, headers = None, None
+        model_version = st.selectbox(
+            label="Model Version", options=["v9", "v10", "v11"]
         )
+        if model_version:
+            endpoint, headers = model_to_endpoints_and_headers[model_version]
+        test_case_selection = st.selectbox(
+            label="Test Cases", options=test_case_options
+        )
+        single_sentence_mode = st.selectbox(
+            label="Split into single sententces", options=["Yes", "No"]
+        )
+        sentence_limit = st.slider(
+            label="Max number of sentences to generate. 500 sentences results in a ~200mb zip file",
+            min_value=1,
+            max_value=500,
+            value=250,
+        )
+        if test_case_selection and model_version:
+            texts = V11_TEST_CASES[test_case_selection]
+            if single_sentence_mode == "Yes":
+                texts = [
+                    sentence
+                    for i in texts
+                    for sentence in i.split(".")
+                    if len(sentence.strip()) > 1
+                ]
+        if st.form_submit_button("Generate audio!"):
+            # Generate audio. This could be sped up with multiprocessing, but the API is limited to
+            # 2.5 requests per second
+            total_tasks = len(texts) * len(speakers_in_v9_v10_v11)
+            total_tasks = (
+                sentence_limit if total_tasks > sentence_limit else total_tasks
+            )
+            current_task = 0
+            metadata = []
+            download_paths = []
+            st.write(f"Total audio files: {total_tasks}")
+            pbar = st.progress(0, text="Generating...")
+            for text in texts:
+                for speaker in speakers_in_v9_v10_v11:
+                    if current_task < total_tasks:
+                        speaker_id = speakers_in_v9_v10_v11[speaker]
+                        audio_file_name = (
+                            f"{model_version}_{speaker}_{text[:15]}.wav"
+                        )
+                        try:
+                            resp = query_tts_api(
+                                speaker_id=speaker_id,
+                                text=text,
+                                style=speaker,
+                                endpoint=endpoint,
+                                headers=headers,
+                            )
+                        except requests.exceptions.ConnectionError:
+                            st.write(
+                                "Connection error encountered, trying once more in 5s"
+                            )
+                            time.sleep(5)
+                            resp = query_tts_api(
+                                speaker_id=speaker_id,
+                                text=text,
+                                style=speaker,
+                                endpoint=endpoint,
+                                headers=headers,
+                            )
+                        if resp.status_code == 200:
+                            with tempfile.NamedTemporaryFile(
+                                mode="wb", suffix=".wav"
+                            ) as fp:
+                                fp.write(resp.content)
+                                fp.seek(0)
+                                audio_array, sample_rate = sf.read(
+                                    fp.name, dtype="float32"
+                                )
+                            path = audio_to_web_path(
+                                audio_array, name=audio_file_name
+                            )
+                            download_paths.append(path)
+                            file_data = {
+                                "Speaker": "".join(speaker.split("_")[0:1]),
+                                "Style": speaker.split("_")[-1],
+                                "Script": text,
+                                "Vote": [],
+                                "Note": "",
+                                "Session": "",
+                                "Dialect": "",
+                                "Audio": audio_file_name,
+                            }
+                            metadata.append(file_data)
+                        else:
+                            msg = (
+                                f"Received status code {resp.status_code} while generating "
+                                f"{audio_file_name}. Full content: {resp.content}"
+                            )
+                            st.write(msg)
+                        current_task += 1
+                        progress_pct = round(current_task / total_tasks, 4)
+                        progress_txt = (
+                            f"Generating... {progress_pct * 100}% complete"
+                        )
+                        pbar.progress(progress_pct, text=progress_txt)
+
+            # Create metadata csv and save zipped audio
+            metadata_path = make_temp_web_dir() / "metadata.csv"
+            metadata_path.write_text(pd.DataFrame(metadata).to_csv())
+            download_paths.append(metadata_path)
+            st_download_files(
+                f"{model_version}-{test_case_selection}-audio-and-metadata.zip",
+                "💾  DOWNLOAD  💾",
+                download_paths,
+            )
 
 
 if __name__ == "__main__":
