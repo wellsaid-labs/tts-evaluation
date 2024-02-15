70d83140ba6a29a61f03171f300b9f11754eeb6c
Jordan Hoskins <jordan@Jordans-Laptop.local>
2024-02-15 14:30:40 -0700

    add test cases containing respellings

---
+++ generate/_test_cases/respellings.py
@@ -0,0 +1,263 @@
+MISPRONOUNCED = {
+    "acetaminophen": (
+        "<respell value='uh-SEE-tuh-MIH-nuh-fuhn'>acetaminophen</respell>",
+        "::uh-SEE-tuh-MIH-nuh-fuhn::",
+    ),
+    "adobe": ("<respell value='uh-DOH-bee'>adobe</respell>", "::uh-DOH-bee::"),
+    "antihistamines": (
+        "<respell value='AN-tee-HIH-stuh-muhnz'>antihistamines</respell>",
+        "::AN-tee-HIH-stuh-muhnz::",
+    ),
+    "asphalt": ("<respell value='AS-fawlt'>asphalt</respell>", "::AS-fawlt::"),
+    "asthma": ("<respell value='AZ-muh'>asthma</respell>", "::AZ-muh::"),
+    "asymptomatic": (
+        "<respell value='AY-SIHMP-tuh-MA-tihk'>asymptomatic</respell>",
+        "::AY-SIHMP-tuh-MA-tihk::",
+    ),
+    "bluish": ("<respell value='BLOO-ihsh'>bluish</respell>", "::BLOO-ihsh::"),
+    "crises": ("<respell value='KRY-seez'>crises</respell>", "::KRY-seez::"),
+    "denials": (
+        "<respell value='dih-NY-uhlz'>denials</respell>",
+        "::dih-NY-uhlz::",
+    ),
+    "deny": ("<respell value='dih-NY'>deny</respell>", "::dih-NY::"),
+    "duo": ("<respell value='DOO-oh'>duo</respell>", "::DOO-oh::"),
+    "enrolled": (
+        "<respell value='ehn-ROHLD'>enrolled</respell>",
+        "::ehn-ROHLD::",
+    ),
+    "flange": ("<respell value='FLANJ'>flange</respell>", "::FLANJ::"),
+    "goldilocks": (
+        "<respell value='GOHL-dee-lawks'>goldilocks</respell>",
+        "::GOHL-dee-lawks::",
+    ),
+    "hypoglycemic": (
+        "<respell value='HY-poh-gly-SEE-mihk'>hypoglycemic</respell>",
+        "::HY-poh-gly-SEE-mihk::",
+    ),
+    "icon": ("<respell value='Y-kahn'>icon</respell>", "::Y-kahn::"),
+    "incorporate": (
+        "<respell value='ihn-KOR-pur-ayt'>incorporate</respell>",
+        "::ihn-KOR-pur-ayt::",
+    ),
+    "iodine": ("<respell value='Y-uh-dyn'>iodine</respell>", "::Y-uh-dyn::"),
+    "karaoke": (
+        "<respell value='KEH-ree-OH-kee'>karaoke</respell>",
+        "::KEH-ree-OH-kee::",
+    ),
+    "meteor": (
+        "<respell value='MEE-tee-ur'>meteor</respell>",
+        "::MEE-tee-ur::",
+    ),
+    "niche": ("<respell value='NIHCH'>niche</respell>", "::NIHCH::"),
+    "nomenclature": (
+        "<respell value='NOH-muhn-klay-chur'>nomenclature</respell>",
+        "::NOH-muhn-klay-chur::",
+    ),
+    "omit": ("<respell value='oh-MIHT'>omit</respell>", "::oh-MIHT::"),
+    "oodles": ("<respell value='OO-duhlz'>oodles</respell>", "::OO-duhlz::"),
+    "osteoporosis": (
+        "<respell value='AW-stee-OH-pur-OH-sihs'>osteoporosis</respell>",
+        "::AW-stee-OH-pur-OH-sihs::",
+    ),
+    "pancreas": (
+        "<respell value='PAN-kree-uhs'>pancreas</respell>",
+        "::PAN-kree-uhs::",
+    ),
+    "quarantine": (
+        "<respell value='KWOH-ruhn-teen'>quarantine</respell>",
+        "::KWOH-ruhn-teen::",
+    ),
+    "realm": ("<respell value='REHLM'>realm</respell>", "::REHLM::"),
+    "reconnaissance": (
+        "<respell value='ree-KAH-nuh-suhns'>reconnaissance</respell>",
+        "::ree-KAH-nuh-suhns::",
+    ),
+    "rendezvous": (
+        "<respell value='RAHN-dih-voo'>rendezvous</respell>",
+        "::RAHN-dih-voo::",
+    ),
+    "ricocheted": (
+        "<respell value='RIH-kuh-shayd'>ricocheted</respell>",
+        "::RIH-kuh-shayd::",
+    ),
+    "scarce": ("<respell value='SKERRS'>scarce</respell>", "::SKERRS::"),
+    "sew": ("<respell value='SOH'>sew</respell>", "::SOH::"),
+    "signage": ("<respell value='SY-nihj'>signage</respell>", "::SY-nihj::"),
+    "sterile": (
+        "<respell value='STEH-ruhl'>sterile</respell>",
+        "::STEH-ruhl::",
+    ),
+    "teammate": (
+        "<respell value='TEE-mayt'>teammate</respell>",
+        "::TEE-mayt::",
+    ),
+    "trachea": (
+        "<respell value='TRAY-kee-uh'>trachea</respell>",
+        "::TRAY-kee-uh::",
+    ),
+    "treachery": (
+        "<respell value='TREH-chur-ee'>treachery</respell>",
+        "::TREH-chur-ee::",
+    ),
+    "triage": ("<respell value='TREE-ahj'>triage</respell>", "::TREE-ahj::"),
+    "verbatim": (
+        "<respell value='vur-BAY-tuhm'>verbatim</respell>",
+        "::vur-BAY-tuhm::",
+    ),
+    "feisty": ("<respell value='FY-stee'>feisty</respell>", " ::FY-stee::"),
+    "corps": ("<respell value='KOR'>corps</respell>", "::KOR::"),
+    "ague": ("<respell value='AY-gyoo'>ague</respell>", "::AY-gyoo::"),
+    "saga": ("<respell value='SAH-guh'>saga</respell>", "::SAH-guh::"),
+    "heist": ("<respell value='HYST'>heist</respell>", "::HYST::"),
+    "overzealous": (
+        "<respell value='OH-vur-ZEH-luhs'>overzealous</respell>",
+        "::OH-vur-ZEH-luhs::",
+    ),
+    "zeal": ("<respell value='ZEEL'>zeal</respell>", "::ZEEL::"),
+    "juxtaposition": (
+        "<respell value='JUHK-stuh-puh-ZIH-shuhn'>juxtaposition</respell>",
+        "::JUHK-stuh-puh-ZIH-shuhn::",
+    ),
+    "overexaggerated": (
+        "<respell value='OH-vur-ihg-ZA-juh-ray-duhd'>overexaggerated</respell>",
+        "::OH-vur-ihg-ZA-juh-ray-duhd::",
+    ),
+    "onomatopoeia": (
+        "<respell value='AH-nuh-MAH-duh-PEE-uh'>onomatopoeia</respell>",
+        "::AH-nuh-MAH-duh-PEE-uh::",
+    ),
+    "California": (
+        "<respell value='KA-luh-FOR-nyuh'>California</respell>",
+        "::KA-luh-FOR-nyuh::",
+    ),
+    "Indian": (
+        "<respell value='IHN-dee-uhn'>Indian</respell>",
+        "::IHN-dee-uhn::",
+    ),
+    "Italian": (
+        "<respell value='ih-TA-lyuhn'>Italian</respell>",
+        "::ih-TA-lyuhn::",
+    ),
+    "application": (
+        "<respell value='A-pluh-KAY-shuhn'>application</respell>",
+        "::A-pluh-KAY-shuhn::",
+    ),
+    "authority": (
+        "<respell value='uh-THOH-ruh-tee'>authority</respell>",
+        "::uh-THOH-ruh-tee::",
+    ),
+    "becoming": (
+        "<respell value='bih-KUH-mihng'>becoming</respell>",
+        "::bih-KUH-mihng::",
+    ),
+    "board": ("<respell value='BORD'>board</respell>", "::BORD::"),
+    "car": ("<respell value='KAR'>car</respell>", "::KAR::"),
+    "civil": ("<respell value='SIH-vuhl'>civil</respell>", "::SIH-vuhl::"),
+    "cover": ("<respell value='KUH-vur'>cover</respell>", "::KUH-vur::"),
+    "critical": (
+        "<respell value='KRIH-tih-kuhl'>critical</respell>",
+        "::KRIH-tih-kuhl::",
+    ),
+    "decision": (
+        "<respell value='dih-SIH-zhuhn'>decision</respell>",
+        "::dih-SIH-zhuhn::",
+    ),
+    "determined": (
+        "<respell value='dih-TUR-muhnd'>determined</respell>",
+        "::dih-TUR-muhnd::",
+    ),
+    "develop": (
+        "<respell value='dih-VEH-luhp'>develop</respell>",
+        "::dih-VEH-luhp::",
+    ),
+    "device": ("<respell value='dih-VYS'>device</respell>", "::dih-VYS::"),
+    "distribution": (
+        "<respell value='DIHS-truh-BYOO-shuhn'>distribution</respell>",
+        "::DIHS-truh-BYOO-shuhn::",
+    ),
+    "efforts": ("<respell value='EH-furts'>efforts</respell>", "::EH-furts::"),
+    "element": (
+        "<respell value='EH-luh-muhnt'>element</respell>",
+        "::EH-luh-muhnt::",
+    ),
+    "famous": ("<respell value='FAY-muhs'>famous</respell>", "::FAY-muhs::"),
+    "finally": (
+        "<respell value='FY-nuh-lee'>finally</respell>",
+        "::FY-nuh-lee::",
+    ),
+    "fully": ("<respell value='FUU-lee'>fully</respell>", "::FUU-lee::"),
+    "gives": ("<respell value='GIHVZ'>gives</respell>", "::GIHVZ::"),
+    "giving": ("<respell value='GIH-vihng'>giving</respell>", "::GIH-vihng::"),
+    "growing": (
+        "<respell value='GROH-ihng'>growing</respell>",
+        "::GROH-ihng::",
+    ),
+    "hard": ("<respell value='HARD'>hard</respell>", "::HARD::"),
+    "ideas": ("<respell value='y-DEE-uhz'>ideas</respell>", "::y-DEE-uhz::"),
+    "instance": (
+        "<respell value='IHN-stuhns'>instance</respell>",
+        "::IHN-stuhns::",
+    ),
+    "instead": (
+        "<respell value='ihn-STEHD'>instead</respell>",
+        "::ihn-STEHD::",
+    ),
+    "leaving": (
+        "<respell value='LEE-vihng'>leaving</respell>",
+        "::LEE-vihng::",
+    ),
+    "moving": ("<respell value='MOO-vihng'>moving</respell>", "::MOO-vihng::"),
+    "musical": (
+        "<respell value='MYOO-zih-kuhl'>musical</respell>",
+        "::MYOO-zih-kuhl::",
+    ),
+    "notes": ("<respell value='NOHTS'>notes</respell>", "::NOHTS::"),
+    "nuclear": (
+        "<respell value='NOOK-lee-ur'>nuclear</respell>",
+        "::NOOK-lee-ur::",
+    ),
+    "ordered": ("<respell value='OR-durd'>ordered</respell>", "::OR-durd::"),
+    "output": ("<respell value='OWT-puut'>output</respell>", "::OWT-puut::"),
+    "phase": ("<respell value='FAYZ'>phase</respell>", "::FAYZ::"),
+    "plants": ("<respell value='PLANTS'>plants</respell>", "::PLANTS::"),
+    "previously": (
+        "<respell value='PREE-vee-uhs-lee'>previously</respell>",
+        "::PREE-vee-uhs-lee::",
+    ),
+    "purpose": ("<respell value='PUR-puhs'>purpose</respell>", "::PUR-puhs::"),
+    "quite": ("<respell value='KWYT'>quite</respell>", "::KWYT::"),
+    "race": ("<respell value='RAYS'>race</respell>", "::RAYS::"),
+    "relations": (
+        "<respell value='ree-LAY-shuhnz'>relations</respell>",
+        "::ree-LAY-shuhnz::",
+    ),
+    "responsible": (
+        "<respell value='ree-SPAHN-suh-buhl'>responsible</respell>",
+        "::ree-SPAHN-suh-buhl::",
+    ),
+    "risk": ("<respell value='RIHSK'>risk</respell>", "::RIHSK::"),
+    "rock": ("<respell value='RAHK'>rock</respell>", "::RAHK::"),
+    "sets": ("<respell value='SEHTS'>sets</respell>", "::SEHTS::"),
+    "share": ("<respell value='SHERR'>share</respell>", "::SHERR::"),
+    "shot": ("<respell value='SHAHT'>shot</respell>", "::SHAHT::"),
+    "situation": (
+        "<respell value='SIH-choo-AY-shuhn'>situation</respell>",
+        "::SIH-choo-AY-shuhn::",
+    ),
+    "something": (
+        "<respell value='SUHM-thihng'>something</respell>",
+        "::SUHM-thihng::",
+    ),
+    "table": ("<respell value='TAY-buhl'>table</respell>", "::TAY-buhl::"),
+    "techniques": (
+        "<respell value='tehk-NEEKS'>techniques</respell>",
+        "::tehk-NEEKS::",
+    ),
+    "unique": ("<respell value='yoo-NEEK'>unique</respell>", "::yoo-NEEK::"),
+    "vote": ("<respell value='VOHT'>vote</respell>", "::VOHT::"),
+    "weeks": ("<respell value='WEEKS'>weeks</respell>", "::WEEKS::"),
+    "win": ("<respell value='WIHN'>win</respell>", "::WIHN::"),
+    "workers": ("<respell value='WUR-kurz'>workers</respell>", "::WUR-kurz::"),
+    "world": ("<respell value='WURLD'>world</respell>", "::WURLD::"),
+}
